9836f2d6652d1ec1798a363ad049198d4de97528
adammeyer <adam.meyer@cru.org>
2016-07-22 09:45:42 -0400

    help component

--- src/app/checkout/checkout.component.js
+++ src/app/checkout/checkout.component.js
@@ -5,6 +5,7 @@ import step1 from './step-1/step-1.component';
 import step2 from './step-2/step-2.component';
 import step3 from './step-3/step-3.component';
 import thankYou from './thank-you/thank-you.component';
+import help from './help/help.component';
 
 import cartService from 'common/services/api/cart.service';
 import designationsService from 'common/services/api/designations.service';
@@ -55,6 +56,7 @@ export default angular
     step2.name,
     step3.name,
     thankYou.name,
+    help.name,
     cartService.name,
     designationsService.name
   ])
