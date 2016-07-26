e4e5cbba05fddd866352f5673d7475459dd327a1
Scotty Waggoner <ozzieorca@gmail.com>
2016-07-26 00:15:27 -0700

    Add order service
    - Add getCurrentPayments() to order service to load links to payment forms
    - Add addBankAccountPayment() with temporary data

--- src/app/checkout/step-2/step-2.component.js
+++ src/app/checkout/step-2/step-2.component.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import paymentEncryptionService from 'common/services/paymentEncryption.service';
+import orderService from 'common/services/api/order.service';
 
 import bankAccount from './bank-account/bank-account.component';
 import creditCard from './credit-card/credit-card.component';
@@ -11,12 +12,30 @@ let componentName = 'checkoutStep2';
 class Step2Controller{
 
   /* @ngInject */
-  constructor(paymentEncryptionService){
+  constructor(paymentEncryptionService, orderService, apiService, hateoasHelperService, $log){
     this.paymentEncryption = paymentEncryptionService;
+    this.orderService = orderService;
+    this.apiService = apiService;
+    this.hateoasHelperService = hateoasHelperService;
+    this.$log = $log;
 
     this.paymentType = 'bankAccount';
+
+    this.savePayment();
   }
 
+  savePayment(){
+    this.orderService.addBankAccountPayment({
+        'account-type': 'checking',
+        'bank-name': 'some bank',
+        'display-account-number': 'xxxxxx-1234',
+        'encrypted-account-number': '123456789012',
+        'routing-number': 123456789
+      })
+      .subscribe((data) => {
+        this.$log.info('added bank account', data);
+      });
+  }
 }
 
 export default angular
@@ -24,7 +43,8 @@ export default angular
     template.name,
     bankAccount.name,
     creditCard.name,
-    paymentEncryptionService.name
+    paymentEncryptionService.name,
+    orderService.name
   ])
   .component(componentName, {
     controller: Step2Controller,
---
+++ src/common/services/api/order.service.js
@@ -0,0 +1,57 @@
+import angular from 'angular';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/mergeMap';
+
+import apiService from '../api.service';
+import hateoasHelperService from 'common/services/hateoasHelper.service';
+
+let serviceName = 'orderService';
+
+class Order{
+
+  /*@ngInject*/
+  constructor(apiService, hateoasHelperService){
+    this.apiService = apiService;
+    this.hateoasHelperService = hateoasHelperService;
+  }
+
+  getCurrentPayments(){
+    if(this.paymentTypes){
+      return Observable.of(this.paymentTypes);
+    }else{
+      return this.apiService.get({
+          path: ['carts', this.apiService.scope, 'default'],
+          zoom: {
+            bankAccount: 'order:paymentmethodinfo:bankaccountform',
+            creditCard: 'order:paymentmethodinfo:creditcardform'
+          }
+        })
+        .do((data) => {
+          this.paymentTypes = data;
+        });
+    }
+  }
+
+  addBankAccountPayment(paymentInfo){
+    return this.getCurrentPayments()
+      .mergeMap((data) => {
+        return this.apiService.post({
+            path: this.hateoasHelperService.getLink(data.bankAccount, 'createbankaccountfororderaction'),
+            data: paymentInfo,
+            followLocation: true
+          });
+      });
+  }
+
+  addCreditCardPayment(){
+
+  }
+
+}
+
+export default angular
+  .module(serviceName, [
+    apiService.name,
+    hateoasHelperService.name
+  ])
+  .service(serviceName, Order);
