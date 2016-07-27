814296a25ae98e9a534f8bd0a37721a721bcee0a
Scotty Waggoner <ozzieorca@gmail.com>
2016-07-26 22:26:49 -0700

    Implement methods of paymentEncryption service

--- src/common/services/paymentEncryption.service.js
+++ src/common/services/paymentEncryption.service.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import _ from 'lodash';
 import ccp from 'common/lib/ccp';
 import { ccpKey } from 'common/app.constants';
 
@@ -9,31 +10,51 @@ let serviceName = 'paymentEncryptionService';
 class PaymentEncryption {
 
   constructor(){
-    this.ccp = ccp.initialize(ccpKey);
+    ccp.initialize(ccpKey);
   }
 
   validateRoutingNumber(routingNumber){
-    return false;
+    routingNumber = this.stripNonNumbers(routingNumber);
+    if(routingNumber.length !== 9){
+      return false;
+    }
+    let digits = routingNumber.split('');
+    let multipliers = [3,7,1,3,7,1,3,7,1];
+
+    let sum = _(_.zip(digits, multipliers))
+      .map((array) => {
+        return array[0] * array[1];
+      })
+      .sum();
+    return sum % 10 === 0;
   }
 
   validateAccountNumber(accountNumber){
-    return false;
+    return this.stripNonNumbers(accountNumber).length > 0;
   }
 
-  validateCardNumber(cardnumber){
-    return false;
+  validateCardNumber(cardNumber){
+    return ccp.validateCardNumber(cardNumber) === null;
   }
 
   validateCardSecurityCode(securityCode){
-    return false;
+    return ccp.validateCardSecurityCode(securityCode) === null;
   }
 
-  getCardType(cardnumber){
-    return '';
+  getCardType(cardNumber){
+    return ccp.getCardType(cardNumber);
   }
 
   encrypt(number){
-    return 0;
+    return ccp.encrypt(number);
+  }
+
+  stripNonNumbers(number){
+    if(number === undefined || number === null){
+      number  = '';
+    }
+    number = number.toString();
+    return number.replace(/\D/g, '');
   }
 
 }
--- src/common/services/paymentEncryption.spec.js
+++ src/common/services/paymentEncryption.spec.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import 'angular-mocks';
 import module from './paymentEncryption.service';
 
-describe('payment encryption service', function() {
+describe('payment encryption service', () => {
   beforeEach(angular.mock.module(module.name));
   var self = {};
 
@@ -10,7 +10,95 @@ describe('payment encryption service', function() {
     self.paymentEncryptionService = paymentEncryptionService;
   }));
 
-  it('to be defined', function() {
+  it('to be defined', () => {
     expect(self.paymentEncryptionService).toBeDefined();
+  });
+
+  describe('validateRoutingNumber', () => {
+    it('to return false for numbers shorter than 9', () => {
+      expect(self.paymentEncryptionService.validateRoutingNumber(1)).toEqual(false);
+      expect(self.paymentEncryptionService.validateRoutingNumber(12345678)).toEqual(false);
+    });
+    it('to return false for numbers longer than 9', () => {
+      expect(self.paymentEncryptionService.validateRoutingNumber(1234567890)).toEqual(false);
+      expect(self.paymentEncryptionService.validateRoutingNumber(1234567890123456)).toEqual(false);
+    });
+    it('to return true for valid routing numbers', () => {
+      expect(self.paymentEncryptionService.validateRoutingNumber(267084131)).toEqual(true);
+      expect(self.paymentEncryptionService.validateRoutingNumber('021300420')).toEqual(true);
+      expect(self.paymentEncryptionService.validateRoutingNumber('043318092')).toEqual(true);
+      expect(self.paymentEncryptionService.validateRoutingNumber(122038251)).toEqual(true);
+    });
+    it('to return false for invalid routing numbers', () => {
+      expect(self.paymentEncryptionService.validateRoutingNumber(267084132)).toEqual(false);
+      expect(self.paymentEncryptionService.validateRoutingNumber(121300420)).toEqual(false);
+      expect(self.paymentEncryptionService.validateRoutingNumber(943318092)).toEqual(false);
+      expect(self.paymentEncryptionService.validateRoutingNumber(122030251)).toEqual(false);
+    });
+  });
+
+  describe('validateAccountNumber', () => {
+    it('to return false for numbers with 0 digits', () => {
+      expect(self.paymentEncryptionService.validateAccountNumber('')).toEqual(false);
+      expect(self.paymentEncryptionService.validateAccountNumber('asdf&*@)njksd')).toEqual(false);
+    });
+    it('to return true for numbers with 1 or more digits', () => {
+      expect(self.paymentEncryptionService.validateAccountNumber(1)).toEqual(true);
+      expect(self.paymentEncryptionService.validateAccountNumber(12345678)).toEqual(true);
+    });
+  });
+
+  describe('validateCardNumber', () => {
+    it('to return false for an invalid number', () => {
+      expect(self.paymentEncryptionService.validateCardNumber('5800000000000000')).toEqual(false);
+    });
+    it('to return true for a valid VISA number', () => {
+      expect(self.paymentEncryptionService.validateCardNumber('4111 1111 1111 1111')).toEqual(true);
+    });
+  });
+
+  describe('validateCardSecurityCode', () => {
+    it('to return false for a code of invalid length', () => {
+      expect(self.paymentEncryptionService.validateCardSecurityCode('12')).toEqual(false);
+      expect(self.paymentEncryptionService.validateCardSecurityCode('12345')).toEqual(false);
+    });
+    it('to return true for a code of valid length', () => {
+      expect(self.paymentEncryptionService.validateCardSecurityCode('123')).toEqual(true);
+      expect(self.paymentEncryptionService.validateCardSecurityCode('1234')).toEqual(true);
+    });
+  });
+
+  describe('getCardType', () => {
+    it('to return the correct card types', () => {
+      expect(self.paymentEncryptionService.getCardType('4024007192720596')).toEqual('VISA');
+      expect(self.paymentEncryptionService.getCardType('5116410773439691')).toEqual('MASTERCARD');
+      expect(self.paymentEncryptionService.getCardType('6011575065399193')).toEqual('DISCOVER');
+      expect(self.paymentEncryptionService.getCardType('373330705156420')).toEqual('AMERICAN_EXPRESS');
+      expect(self.paymentEncryptionService.getCardType('30060165822352')).toEqual('DINERS_CLUB');
+    });
+  });
+
+  describe('encrypt', () => {
+    it('to return the encrypted version of a number', () => {
+      expect(self.paymentEncryptionService.encrypt('4024007192720596').length).toEqual(344);
+      expect(self.paymentEncryptionService.encrypt('5116410773439691').length).toEqual(344);
+      expect(self.paymentEncryptionService.encrypt('123').length).toEqual(344);
+      expect(self.paymentEncryptionService.encrypt('1234').length).toEqual(344);
+      expect(self.paymentEncryptionService.encrypt('123456789').length).toEqual(344);
+    });
+  });
+
+  describe('stripNonNumbers', () => {
+    it('to convert imputs to strings', () => {
+      expect(self.paymentEncryptionService.stripNonNumbers()).toEqual('');
+      expect(self.paymentEncryptionService.stripNonNumbers(null)).toEqual('');
+      expect(self.paymentEncryptionService.stripNonNumbers(1)).toEqual('1');
+      expect(self.paymentEncryptionService.stripNonNumbers(1234567890)).toEqual('1234567890');
+      expect(self.paymentEncryptionService.stripNonNumbers('123456')).toEqual('123456');
+    });
+    it('to remove all non numbers', () => {
+      expect(self.paymentEncryptionService.stripNonNumbers('q2fha3w78')).toEqual('2378');
+      expect(self.paymentEncryptionService.stripNonNumbers('*&^71q@)(h(201JK&@')).toEqual('71201');
+    });
   });
 });
