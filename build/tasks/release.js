b33b7ad4a8667b500265afe51eb85c3065a03f6e
Scotty Waggoner <ozzieorca@gmail.com>
2016-08-08 11:11:08 -0700

    Update inline systemjs bundle paths
    - Remove systemjs-route-bundler that is leftover from swimlane seed
    - Remove window.prod

--- build/tasks/release.js
+++ build/tasks/release.js
@@ -8,7 +8,6 @@ var paths = require('../paths');
 gulp.task('cache-bust', function () {
   var cacheBust = "var systemLocate = System.locate; System.locate = function(load) { var cacheBust = '?bust=' + " + Math.round(new Date() / 1000) +"; return Promise.resolve(systemLocate.call(this, load)).then(function(address) { if (address.indexOf('bust') > -1 || address.indexOf('css') > -1 || address.indexOf('json') > -1) return address; return address + cacheBust; });}\n"
   return gulp.src('dist/app/app.js')
-    .pipe($.insert.prepend("window.prod = true;\n"))
     .pipe($.insert.prepend(cacheBust))
     .pipe(gulp.dest('dist/app'));
 });
@@ -18,7 +17,7 @@ gulp.task('inline-systemjs', function () {
       './jspm_packages/es6-module-loader.js',
       './jspm_packages/system.js',
       './system.config.js',
-      'dist/bundles/main.js'
+      'dist/main.js'
     ])
     //.pipe($.uglify())
     .pipe($.concat('main.js'));
@@ -27,7 +26,7 @@ gulp.task('inline-systemjs', function () {
       './jspm_packages/es6-module-loader.js',
       './jspm_packages/system.js',
       './system.config.js',
-      'dist/bundles/common.js'
+      'dist/common.js'
     ])
     //.pipe($.uglify())
     .pipe($.concat('common.js'));
--- system.config.js
+++ system.config.js
@@ -127,7 +127,7 @@ System.config({
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.7"
+      "process": "npm:process@0.11.8"
     },
     "github:jspm/nodelibs-stream@0.1.0": {
       "stream-browserify": "npm:stream-browserify@1.0.0"
@@ -745,7 +745,7 @@ System.config({
     "npm:process-nextick-args@1.0.7": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:process@0.11.7": {
+    "npm:process@0.11.8": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "vm": "github:jspm/nodelibs-vm@0.1.0"
