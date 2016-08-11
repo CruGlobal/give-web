70bd610e06ebaee5c9b7bdc54420d0cd117b88a4
Scotty Waggoner <ozzieorca@gmail.com>
2016-08-10 23:55:13 -0700

    Step 2 component: add changePaymentType to also set submitted to false and add tests

--- src/app/checkout/step-2/step-2.spec.js
+++ src/app/checkout/step-2/step-2.spec.js
@@ -2,8 +2,8 @@ import angular from 'angular';
 import 'angular-mocks';
 import module from './step-2.component';
 
-describe('checkout', function() {
-  describe('step 2', function() {
+describe('checkout', () => {
+  describe('step 2', () => {
     beforeEach(angular.mock.module(module.name));
     var self = {};
 
@@ -11,12 +11,37 @@ describe('checkout', function() {
       var $scope = $rootScope.$new();
 
       self.controller = $componentController(module.name, {
-        $scope: $scope
-      });
+          $scope: $scope
+        },
+        {
+          changeStep: () => {}
+        });
     }));
 
-    it('to be defined', function() {
-      expect(self.controller).toBeDefined();
+    describe('changePaymentType', () => {
+      it('should set the payment type and set submitted to false', () => {
+        self.controller.submitted = true;
+        self.controller.changePaymentType('creditCard');
+        expect(self.controller.paymentType).toBe('creditCard');
+        expect(self.controller.submitted).toBe(false);
+        self.controller.submitted = true;
+        self.controller.changePaymentType('bankAccount');
+        expect(self.controller.paymentType).toBe('bankAccount');
+        expect(self.controller.submitted).toBe(false);
+      });
+    });
+
+    describe('onSave', () => {
+      it('should set submitted to false if save was unsuccessful', () => {
+        self.controller.submitted = true;
+        self.controller.onSave(false);
+        expect(self.controller.submitted).toBe(false);
+      });
+      it('should call changeStep if save was successful', () => {
+        spyOn(self.controller, 'changeStep');
+        self.controller.onSave(true);
+        expect(self.controller.changeStep).toHaveBeenCalledWith({ newStep: 'review' });
+      });
     });
   });
 });
