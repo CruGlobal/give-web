a339fcd38b6b61a995dacfbc6f80a0fb766bd098
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-08-22 14:22:45 -0400

    Initial sessionModalService.

---
+++ src/common/components/signUpModal/signUpModal.component.js
@@ -0,0 +1,31 @@
+import angular from 'angular';
+import 'angular-gettext';
+import template from './signUpModal.tpl';
+
+let componentName = 'signUpModal';
+
+class SignUpModalController {
+
+  /* @ngInject */
+  constructor( gettext ) {
+    this.gettext = gettext;
+  }
+
+  $onInit() {
+    this.modalTitle = this.gettext( 'Sign Up' );
+  }
+}
+
+export default angular
+  .module( componentName, [
+    'gettext',
+    template.name,
+  ] )
+  .component( componentName, {
+    controller:  SignUpModalController,
+    templateUrl: template.name,
+    bindings:    {
+      modalTitle:    '=',
+      onStateChange: '&'
+    }
+  } );
---
+++ src/common/services/session/sessionModal.controller.js
@@ -0,0 +1,45 @@
+import angular from 'angular';
+
+import signInModal from 'common/components/signInModal/signInModal.component'
+import signUpModal from 'common/components/signUpModal/signUpModal.component'
+import resetPasswordModal from 'common/components/resetPasswordModal/resetPasswordModal.component'
+
+let controllerName = 'sessionModalController';
+
+class SessionModalController {
+
+  /* @ngInject */
+  constructor( $uibModalInstance, sessionService, state ) {
+    this.$uibModalInstance = $uibModalInstance;
+    this.sessionService = sessionService;
+    this.stateChanged( state );
+  }
+
+  stateChanged( state ) {
+    this.state = state;
+  }
+
+  onSignInSuccess() {
+    this.$uibModalInstance.close();
+  }
+
+  onSignUpSuccess() {
+    this.$uibModalInstance.close();
+  }
+
+  onFailure() {
+    this.$uibModalInstance.dismiss('error');
+  }
+
+  onCancel() {
+    this.$uibModalInstance.dismiss('cancel');
+  }
+}
+
+export default angular
+  .module( controllerName, [
+    signInModal.name,
+    signUpModal.name,
+    resetPasswordModal.name
+  ] )
+  .controller( controllerName, SessionModalController );
