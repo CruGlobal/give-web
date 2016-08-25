183d245ae2b0d5cffbee970066a9cf0edfe2618d
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-08-25 10:12:54 -0400

    Fixes issue with signIn Modal closing on invalid user/pass

--- src/common/components/signInModal/signInModal.component.js
+++ src/common/components/signInModal/signInModal.component.js
@@ -42,7 +42,6 @@ export default angular
     bindings:    {
       modalTitle:    '=',
       onStateChange: '&',
-      onSuccess:     '&',
-      onFailure:     '&'
+      onSuccess:     '&'
     }
   } );
