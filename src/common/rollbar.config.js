bd41fa1aeae947d5b0054d4fa3c5bd1fdb3a68c3
Scotty Waggoner <ozzieorca@gmail.com>
2016-08-26 17:46:11 -0700

    Send stack traces to rollbar as actual stack frames instead of strings

--- src/common/rollbar.config.js
+++ src/common/rollbar.config.js
@@ -1,7 +1,9 @@
 import { rollbarAccessToken } from 'common/app.constants';
 import rollbar from 'rollbar-browser';
 import stacktrace from 'stacktrace-js';
+import map from 'lodash/map';
 import defaults from 'lodash/defaults';
+import get from 'lodash/get';
 
 /* @ngInject */
 function rollbarConfig(envServiceProvider, $provide) {
@@ -11,7 +13,8 @@ function rollbarConfig(envServiceProvider, $provide) {
     captureUnhandledRejections: true,
     environment: envServiceProvider.get(),
     enabled: !envServiceProvider.is('development'), // Disable rollbar in development environment
-    verbose: envServiceProvider.is('development') // Log rollbar errors to console in development environment
+    verbose: envServiceProvider.is('development'), // Log rollbar errors to console in development environment
+    transform: transformRollbarPayload
   };
   let Rollbar = rollbar.init(rollbarConfig);
 
@@ -28,19 +31,23 @@ function rollbarConfig(envServiceProvider, $provide) {
 
         // Generate message string
         let message = args
-          .map((arg) => angular.toJson(arg))
+          .map((arg) => {
+            if(arg.message){
+              return arg.message; // Message came from $ExceptionHandler
+            }else{
+              return angular.toJson(arg);
+            }
+          })
           .join(', ');
+
+        let origin = args[0].message ? '$ExceptionHandler' : '$log';
 
         stacktrace.get({offline: true})
           .then((stackFrames) => {
             // Ignore first stack frame which is this function
             stackFrames.shift();
-            // Convert stack trace to string
-            stackFrames = stackFrames.map((sf) => {
-              return '    at ' + sf.toString();
-            }).join('\n');
             // Send combined message and stack trace to rollbar
-            Rollbar[rollbarLogLevel](message + '\n' + stackFrames);
+            Rollbar[rollbarLogLevel](message, {stackTrace: stackFrames, origin: origin});
           })
           .catch((error) => {
             // Send message without stack trace to rollbar
@@ -55,10 +62,39 @@ function rollbarConfig(envServiceProvider, $provide) {
 
     return $delegate;
   });
+
+}
+
+function formatStacktraceForRollbar(stackFrames){
+  return map(stackFrames, (frame) => {
+    return {
+      method: frame.functionName,
+      lineno: frame.lineNumber,
+      colno: frame.columnNumber,
+      filename: frame.fileName
+    };
+  });
+}
+
+function transformRollbarPayload(payload){
+  if(get(payload, 'data.body.message.extra.stackTrace')) {
+    // Convert message format to trace format
+    payload.data.body.trace = {
+      frames: formatStacktraceForRollbar(payload.data.body.message.extra.stackTrace),
+      exception: {
+        message: payload.data.body.message.body,
+        class: payload.data.body.message.extra.origin
+      }
+    };
+    delete payload.data.body.message;
+  }
+  return payload;
 }
 
 export {
   rollbarConfig as default,
   rollbar, // For mocking during testing
-  stacktrace // For mocking during testing
+  stacktrace, // For mocking during testing,
+  formatStacktraceForRollbar, // To test this function separately
+  transformRollbarPayload // To test this function separately
 };
--- src/common/rollbar.config.spec.js
+++ src/common/rollbar.config.spec.js
@@ -11,87 +11,202 @@ import * as module from './rollbar.config';
 describe('rollbarConfig', () => {
   var self = {};
 
-  beforeEach(() => {
-    // Use rollbar config function somewhere
-    angular.module('testRollbarConfig', ['environment'])
-      .config(module.default);
+  describe('pipe $log to Rollbar', () => {
+    beforeEach(() => {
+      // Use rollbar config function somewhere
+      angular.module('testRollbarConfig', ['environment'])
+        .config(module.default);
 
-    // Mock rollbar
-    self.rollbarSpies = {
-      log: jasmine.createSpy('log'),
-      debug: jasmine.createSpy('debug'),
-      info: jasmine.createSpy('info'),
-      warning: jasmine.createSpy('warning'),
-      error: jasmine.createSpy('error')
-    };
-    module.rollbar.init = () => self.rollbarSpies;
+      // Mock rollbar
+      self.rollbarSpies = {
+        log: jasmine.createSpy('log'),
+        debug: jasmine.createSpy('debug'),
+        info: jasmine.createSpy('info'),
+        warning: jasmine.createSpy('warning'),
+        error: jasmine.createSpy('error')
+      };
+      module.rollbar.init = () => self.rollbarSpies;
 
-    // Mock stacktrace
-    self.stacktraceSpy = spyOn(module.stacktrace, 'get').and.callFake(() => Observable.of(['ignored frame', 'first frame']).toPromise());
 
-    // Init and run the test module
-    angular.mock.module('testRollbarConfig');
+      self.rollbarExtraArgs = {
+        stackTrace: [
+          {
+            functionName: 'a',
+            lineNumber: 1,
+            columnNumber: 1,
+            fileName: 'a.js'
+          }
+        ],
+        origin: '$log'
+      };
 
-    inject(($log, $window) => {
-      self.$log = $log;
-      self.$window = $window;
+      // Mock stacktrace
+      self.stacktraceSpy = spyOn(module.stacktrace, 'get').and.callFake(() =>
+        Observable.of(['ignored frame', self.rollbarExtraArgs.stackTrace[0]]).toPromise());
+
+      // Init and run the test module
+      angular.mock.module('testRollbarConfig');
+
+      inject(($log, $window) => {
+        self.$log = $log;
+        self.$window = $window;
+      });
+    });
+
+    it('should send $log.log to rollbar and call through to $log', (done) => {
+      spyOn(self.$log, 'log').and.callThrough();
+      self.$log.log('test log');
+      expect(self.$log.log.logs[0]).toEqual(['test log']);
+      self.$window.setTimeout(() => { // Use setTimout to wait until the event loop has been called once to process the stacktrace promise
+        expect(self.rollbarSpies.log).toHaveBeenCalledWith('"test log"', self.rollbarExtraArgs);
+        done(); // Tell jasmine that our async behavior has finished
+      });
+    });
+    it('should send $log.debug to rollbar and call through to $log', (done) => {
+      spyOn(self.$log, 'debug').and.callThrough();
+      self.$log.debug('test debug');
+      expect(self.$log.debug.logs[0]).toEqual(['test debug']);
+      self.$window.setTimeout(() => {
+        expect(self.rollbarSpies.debug).toHaveBeenCalledWith('"test debug"', self.rollbarExtraArgs);
+        done();
+      });
+    });
+    it('should send $log.info to rollbar and call through to $log', (done) => {
+      spyOn(self.$log, 'info').and.callThrough();
+      self.$log.info('test info');
+      expect(self.$log.info.logs[0]).toEqual(['test info']);
+      self.$window.setTimeout(() => {
+        expect(self.rollbarSpies.info).toHaveBeenCalledWith('"test info"', self.rollbarExtraArgs);
+        done();
+      });
+    });
+    it('should send $log.warn to rollbar and call through to $log', (done) => {
+      spyOn(self.$log, 'warn').and.callThrough();
+      self.$log.warn('test warn');
+      expect(self.$log.warn.logs[0]).toEqual(['test warn']);
+      self.$window.setTimeout(() => {
+        expect(self.rollbarSpies.warning).toHaveBeenCalledWith('"test warn"', self.rollbarExtraArgs);
+        done();
+      });
+    });
+    it('should send $log.error to rollbar and call through to $log', (done) => {
+      spyOn(self.$log, 'error').and.callThrough();
+      self.$log.error('test error');
+      expect(self.$log.error.logs[0]).toEqual(['test error']);
+      self.$window.setTimeout(() => {
+        expect(self.rollbarSpies.error).toHaveBeenCalledWith('"test error"', self.rollbarExtraArgs);
+        done();
+      });
+    });
+    it('should send errors from $ExceptionHandler to rollbar', (done) => {
+      spyOn(self.$log, 'error').and.callThrough();
+      self.$log.error(new Error('some exception'));
+      expect(self.$log.error.logs[0]).toEqual([new Error('some exception')]);
+      self.$window.setTimeout(() => {
+        self.rollbarExtraArgs.origin = '$ExceptionHandler';
+        expect(self.rollbarSpies.error).toHaveBeenCalledWith('some exception', self.rollbarExtraArgs);
+        done();
+      });
+    });
+    it('should send a log to rollbar even the stacktrace fails', (done) => {
+      spyOn(self.$log, 'error').and.callThrough();
+      self.stacktraceSpy.and.callFake(() => Observable.throw('error message when fetching stack').toPromise());
+      self.$log.error('test error');
+      expect(self.$log.error.logs[0]).toEqual(['test error']);
+      self.$window.setTimeout(() => {
+        expect(self.rollbarSpies.error).toHaveBeenCalledWith('"test error"');
+        expect(self.rollbarSpies.warning).toHaveBeenCalledWith('Error loading stackframes: error message when fetching stack');
+        done();
+      });
     });
   });
 
-  it('should send $log.log to rollbar and call through to $log', (done) => {
-    spyOn(self.$log, 'log').and.callThrough();
-    self.$log.log('test log');
-    expect(self.$log.log.logs[0]).toEqual(['test log']);
-    self.$window.setTimeout(() => { // Use setTimout to wait until the event loop has been called once to process the stacktrace promise
-      expect(self.rollbarSpies.log).toHaveBeenCalledWith('"test log"\n    at first frame');
-      done(); // Tell jasmine that our async behavior has finished
+  describe('formatStacktraceForRollbar', () => {
+    it('should rename stack frame object keys', () => {
+      expect(module.formatStacktraceForRollbar([
+        {
+          functionName: 'a',
+          lineNumber: 1,
+          columnNumber: 1,
+          fileName: 'a.js'
+        },
+        {
+          functionName: 'b',
+          lineNumber: 2,
+          columnNumber: 2,
+          fileName: 'b.js'
+        }
+      ]))
+        .toEqual([
+          {
+            method: 'a',
+            lineno: 1,
+            colno: 1,
+            filename: 'a.js'
+          },
+          {
+            method: 'b',
+            lineno: 2,
+            colno: 2,
+            filename: 'b.js'
+          }
+        ]);
     });
   });
-  it('should send $log.debug to rollbar and call through to $log', (done) => {
-    spyOn(self.$log, 'debug').and.callThrough();
-    self.$log.debug('test debug');
-    expect(self.$log.debug.logs[0]).toEqual(['test debug']);
-    self.$window.setTimeout(() => {
-      expect(self.rollbarSpies.debug).toHaveBeenCalledWith('"test debug"\n    at first frame');
-      done();
+
+  describe('transformRollbarPayload', () => {
+    it('should convert the payload from message format to trace format', () => {
+      expect(module.transformRollbarPayload({
+        data: {
+          body: {
+            message: {
+              body: 'some error',
+              extra: {
+                stackTrace: [{
+                  functionName: 'a',
+                  lineNumber: 1,
+                  columnNumber: 1,
+                  fileName: 'a.js'
+                }],
+                origin: '$log'
+              }
+            }
+          }
+        }
+      }))
+        .toEqual({
+          data: {
+            body: {
+              trace: {
+                frames: [{
+                  method: 'a',
+                  lineno: 1,
+                  colno: 1,
+                  filename: 'a.js'
+                }],
+                exception: {
+                  message: 'some error',
+                  class: '$log'
+                }
+              }
+            }
+          }
+        });
     });
-  });
-  it('should send $log.info to rollbar and call through to $log', (done) => {
-    spyOn(self.$log, 'info').and.callThrough();
-    self.$log.info('test info');
-    expect(self.$log.info.logs[0]).toEqual(['test info']);
-    self.$window.setTimeout(() => {
-      expect(self.rollbarSpies.info).toHaveBeenCalledWith('"test info"\n    at first frame');
-      done();
-    });
-  });
-  it('should send $log.warn to rollbar and call through to $log', (done) => {
-    spyOn(self.$log, 'warn').and.callThrough();
-    self.$log.warn('test warn');
-    expect(self.$log.warn.logs[0]).toEqual(['test warn']);
-    self.$window.setTimeout(() => {
-      expect(self.rollbarSpies.warning).toHaveBeenCalledWith('"test warn"\n    at first frame');
-      done();
-    });
-  });
-  it('should send $log.error to rollbar and call through to $log', (done) => {
-    spyOn(self.$log, 'error').and.callThrough();
-    self.$log.error('test error');
-    expect(self.$log.error.logs[0]).toEqual(['test error']);
-    self.$window.setTimeout(() => {
-      expect(self.rollbarSpies.error).toHaveBeenCalledWith('"test error"\n    at first frame');
-      done();
-    });
-  });
-  it('should send a log to rollbar even the stacktrace fails', (done) => {
-    spyOn(self.$log, 'error').and.callThrough();
-    self.stacktraceSpy.and.callFake(() => Observable.throw('error message when fetching stack').toPromise());
-    self.$log.error('test error');
-    expect(self.$log.error.logs[0]).toEqual(['test error']);
-    self.$window.setTimeout(() => {
-      expect(self.rollbarSpies.error).toHaveBeenCalledWith('"test error"');
-      expect(self.rollbarSpies.warning).toHaveBeenCalledWith('Error loading stackframes: error message when fetching stack');
-      done();
+    it('should leave the payload unmodified if extra.stackTrace is missing', () => {
+      let payload = {
+        data: {
+          body: {
+            message: {
+              body: 'some error',
+              extra: {
+                somethingElse: 1
+              }
+            }
+          }
+        }
+      };
+      expect(module.transformRollbarPayload(payload)).toEqual(payload);
     });
   });
 });
