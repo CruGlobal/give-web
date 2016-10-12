0ede786aa263d0f7282a4b688fdbfa16cedea33d
Adam Meyer <adam.meyer@cru.org>
2016-10-12 17:52:17 -0400

    Desktop Nav (#174)
    Desktop Nav

--- src/common/components/nav/nav.component.spec.js
+++ src/common/components/nav/nav.component.spec.js
@@ -12,7 +12,10 @@ describe( 'nav', function () {
 
   beforeEach( inject( function ( _$componentController_, _$document_, _$httpBackend_ ) {
     $ctrl = _$componentController_( module.name,
-      {$window: {location: {href: 'cart.html'}}} );
+      {$window: {
+        location: {href: 'cart.html'},
+        screen: {width: 600}
+    }} );
     $httpBackend = _$httpBackend_;
     $document = _$document_;
   } ) );
@@ -32,6 +35,7 @@ describe( 'nav', function () {
   it('to toggle menu', () => {
     $ctrl.toggleMenu();
     expect( $ctrl.mobileNavOpen ).toEqual( true );
+    expect( $ctrl.desktopSearch ).toEqual( true );
     expect( $document[0].body.className ).toContain('body-scroll-lock');
   });
 
@@ -59,4 +63,108 @@ describe( 'nav', function () {
     $ctrl.cruSearch('hello');
     expect( $ctrl.$window.location.href ).toContain( 'search.hello.html');
   });
+
+  it('to load mobile nav on small screens', () => {
+    $ctrl.$onInit();
+    expect( $ctrl.templateUrl ).toContain( 'mobile');
+  });
+} );
+
+
+describe( 'nav signInButton', function () {
+  beforeEach( angular.mock.module( module.name ) );
+  let $ctrl, $rootScope;
+
+  beforeEach( inject( function ( _$componentController_, _$rootScope_ ) {
+    $rootScope = _$rootScope_;
+    $ctrl = _$componentController_( module.name,
+      {$window: {location: jasmine.createSpyObj( 'location', ['reload'] )}}
+    );
+  } ) );
+
+  it( 'to be defined', function () {
+    expect( $ctrl ).toBeDefined();
+  } );
+
+  describe( 'isSignedIn', () => {
+    describe( 'with \'REGISTERED\' cortex-session', () => {
+      beforeEach( () => {
+        spyOn( $ctrl.sessionService, 'getRole' ).and.returnValue( 'REGISTERED' );
+        $ctrl.$onInit();
+      } );
+
+      afterEach( () => {
+        $ctrl.$onDestroy();
+      } );
+
+      it( 'has true value', () => {
+        expect( $ctrl.isSignedIn ).toEqual( true );
+      } );
+    } );
+
+    describe( 'with \'GUEST\' cortex-session', () => {
+      beforeEach( () => {
+        spyOn( $ctrl.sessionService, 'getRole' ).and.returnValue( 'GUEST' );
+        $ctrl.$onInit();
+      } );
+
+      afterEach( () => {
+        $ctrl.$onDestroy();
+      } );
+
+      it( 'has false value', () => {
+        expect( $ctrl.isSignedIn ).toEqual( false );
+      } );
+    } );
+  } );
+
+  describe( 'signIn', () => {
+    let deferred;
+    beforeEach( inject( function ( _$q_ ) {
+      deferred = _$q_.defer();
+      spyOn( $ctrl.sessionModalService, 'signIn' ).and.callFake( () => deferred.promise );
+      $ctrl.signIn();
+    } ) );
+
+    it( 'opens signIn Modal', () => {
+      expect( $ctrl.sessionModalService.signIn ).toHaveBeenCalled();
+    } );
+
+    it( 'reloads window on success', () => {
+      deferred.resolve();
+      $rootScope.$digest();
+      expect( $ctrl.$window.location.reload ).toHaveBeenCalled();
+    } );
+
+    it( 'does nothing on failure', () => {
+      deferred.reject();
+      $rootScope.$digest();
+      expect( $ctrl.$window.location.reload ).not.toHaveBeenCalled();
+    } );
+  } );
+
+  describe( 'signOut', () => {
+    let deferred;
+    beforeEach( inject( function ( _$q_ ) {
+      deferred = _$q_.defer();
+      spyOn( $ctrl.sessionService, 'signOut' ).and.callFake( () => deferred.promise );
+      $ctrl.signOut();
+    } ) );
+
+    it( 'calls sessionService.signOut', () => {
+      expect( $ctrl.sessionService.signOut ).toHaveBeenCalled();
+    } );
+
+    it( 'reloads window on success', () => {
+      deferred.resolve();
+      $rootScope.$digest();
+      expect( $ctrl.$window.location.reload ).toHaveBeenCalled();
+    } );
+
+    it( 'does nothing on failure', () => {
+      deferred.reject();
+      $rootScope.$digest();
+      expect( $ctrl.$window.location.reload ).not.toHaveBeenCalled();
+    } );
+  } );
 } );
