fd1ccbe0724cbf3b9007e3dc435e187437641383
adammeyer <adam.meyer@cru.org>
2016-10-12 14:57:20 -0400

    Desktop Nav

--- src/common/components/nav/nav.component.js
+++ src/common/components/nav/nav.component.js
@@ -1,26 +1,34 @@
 import angular from 'angular';
 import transform from 'lodash/transform';
 import isObject from 'lodash/isObject';
+import includes from 'lodash/includes';
+
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import cartService from 'common/services/api/cart.service';
+import sessionService from 'common/services/session/session.service';
+import sessionModalService from 'common/services/session/sessionModal.service';
 import loadingComponent from 'common/components/loading/loading.component';
-import signInButton from 'common/components/signInButton/signInButton.component';
 import mobileNavLevelComponent from './navMobileLevel.component';
 
 import mobileTemplate from './mobileNav.tpl';
+import desktopTemplate from './desktopNav.tpl';
 
 let componentName = 'cruNav';
 
 class NavController{
 
   /* @ngInject */
-  constructor($http, $document, $window, envService, cartService){
+  constructor($http, $document, $window, envService, cartService, sessionService, sessionModalService){
     this.$http = $http;
     this.$document = $document;
     this.$window = $window;
+
     this.cartService = cartService;
+    this.sessionService = sessionService;
+    this.sessionModalService = sessionModalService;
+
     this.imgDomain = envService.read('imgDomain');
     this.navFeed = envService.read('navFeed');
   }
@@ -37,6 +45,34 @@ class NavController{
     this.getNav().subscribe((structure) => {
       this.menuStructure = structure;
     });
+
+    this.subscription = this.sessionService.sessionSubject.subscribe( () => this.sessionChanged() );
+
+    this.templateUrl = this.$window.screen && this.$window.screen.width < 991 ? mobileTemplate.name : desktopTemplate.name;
+  }
+
+  $onDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  signIn() {
+    this.sessionModalService
+      .signIn()
+      .then( () => {
+        this.$window.location.reload();
+      } );
+  }
+
+  signOut() {
+    this.sessionService
+      .signOut()
+      .then( () => {
+        this.$window.location.reload();
+      } );
+  }
+
+  sessionChanged() {
+    this.isSignedIn = includes( ['IDENTIFIED', 'REGISTERED'], this.sessionService.getRole() );
   }
 
   getNav() {
@@ -77,6 +113,7 @@ class NavController{
   }
 
   loadCart() {
+    this.cartData = null;
     this.cartService.get()
       .subscribe( ( data ) => {
         this.cartData = data;
@@ -85,6 +122,7 @@ class NavController{
 
   toggleMenu(){
     this.mobileNavOpen = !this.mobileNavOpen;
+    this.desktopSearch = !this.desktopSearch;
 
     var body = angular.element(this.$document[0].body);
     body.toggleClass('body-scroll-lock');
@@ -99,12 +137,14 @@ export default angular
   .module(componentName, [
     'environment',
     mobileTemplate.name,
+    desktopTemplate.name,
     cartService.name,
     loadingComponent.name,
-    signInButton.name,
+    sessionService.name,
+    sessionModalService.name,
     mobileNavLevelComponent.name
   ])
   .component(componentName, {
     controller: NavController,
-    templateUrl: mobileTemplate.name
+    template: '<ng-include src="$ctrl.templateUrl"></ng-include>'
   });
