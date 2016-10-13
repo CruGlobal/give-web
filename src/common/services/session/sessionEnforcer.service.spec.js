1343d8b35551b223142deea8f6f65e5bd6631eb4
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-10-13 15:11:10 -0400

    Added donor mode to sessionEnforcer

--- src/common/services/session/sessionEnforcer.service.spec.js
+++ src/common/services/session/sessionEnforcer.service.spec.js
@@ -1,11 +1,25 @@
 import angular from 'angular';
 import 'angular-mocks';
-import module from './sessionEnforcer.service';
+import module, {EnforcerCallbacks, EnforcerModes} from './sessionEnforcer.service';
 import {Roles} from 'common/services/session/session.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
 
 describe( 'sessionEnforcerService', function () {
   beforeEach( angular.mock.module( module.name ) );
-  let sessionEnforcerService, sessionModalService, sessionService, deferred;
+  let sessionEnforcerService,
+    sessionModalService,
+    sessionService,
+    deferred,
+    registerAccountService;
+
+  beforeEach( () => {
+    registerAccountService = jasmine.createSpy( 'registerAccountService' );
+    angular.mock.module( ( $provide ) => {
+      $provide.value( 'registerAccountService', registerAccountService );
+    } );
+  } );
 
   beforeEach( inject( function ( _sessionEnforcerService_, _sessionModalService_, _sessionService_, _$q_ ) {
     sessionEnforcerService = _sessionEnforcerService_;
@@ -35,45 +49,250 @@ describe( 'sessionEnforcerService', function () {
     it( 'accepts \'sign-in\', \'cancel\' and \'change\' callbacks', () => {
       expect( sessionEnforcerService(
         [Roles.public, Roles.registered], {
-          'sign-in': () => {
+          [EnforcerCallbacks.signIn]: () => {
           },
-          cancel:    () => {
+          [EnforcerCallbacks.cancel]: () => {
           },
-          change:    () => {
+          [EnforcerCallbacks.change]: () => {
           }
         }
       ) ).toEqual( jasmine.any( String ) );
       expect( sessionModalService.open ).not.toHaveBeenCalled();
     } );
 
-    describe( 'does not include current role', () => {
-      let signIn, cancel, change, $rootScope;
-      beforeEach( inject( function ( _$rootScope_ ) {
-        $rootScope = _$rootScope_;
-        signIn = jasmine.createSpy( 'success' );
-        cancel = jasmine.createSpy( 'failure' );
-        change = jasmine.createSpy( 'change' );
-        sessionEnforcerService( [Roles.registered], {'sign-in': signIn, cancel: cancel, change: change} );
-      } ) );
+    describe( '\'session\' mode', () => {
+      describe( 'does not include current role', () => {
+        let signIn, cancel, change, $rootScope;
+        beforeEach( inject( function ( _$rootScope_ ) {
+          $rootScope = _$rootScope_;
+          signIn = jasmine.createSpy( 'success' );
+          cancel = jasmine.createSpy( 'failure' );
+          change = jasmine.createSpy( 'change' );
+          sessionEnforcerService( [Roles.registered], {
+            [EnforcerCallbacks.signIn]: signIn,
+            [EnforcerCallbacks.cancel]: cancel,
+            [EnforcerCallbacks.change]: change
+          }, EnforcerModes.session );
+        } ) );
 
-      it( 'opens sign-in modal and calls \'change\' callback', () => {
-        expect( change ).toHaveBeenCalledWith( Roles.public );
-        expect( sessionModalService.open ).toHaveBeenCalledWith( 'sign-in', {backdrop: 'static', keyboard: false} );
-      } );
+        it( 'opens sign-in modal and calls \'change\' callback', () => {
+          expect( change ).toHaveBeenCalledWith( Roles.public );
+          expect( sessionModalService.open ).toHaveBeenCalledWith( 'sign-in', {backdrop: 'static', keyboard: false} );
+        } );
 
-      describe( 'sign-in success', () => {
-        it( 'calls \'sign-in\' callback', () => {
-          deferred.resolve();
-          $rootScope.$digest();
-          expect( signIn ).toHaveBeenCalled();
+        describe( 'sign-in success', () => {
+          it( 'calls \'sign-in\' callback', () => {
+            deferred.resolve();
+            $rootScope.$digest();
+            expect( signIn ).toHaveBeenCalled();
+          } );
+        } );
+
+        describe( 'sign-in canceled', () => {
+          it( 'calls \'cancel\' callback', () => {
+            deferred.reject();
+            $rootScope.$digest();
+            expect( cancel ).toHaveBeenCalled();
+          } );
         } );
       } );
 
-      describe( 'sign-in canceled', () => {
-        it( 'calls \'cancel\' callback', () => {
-          deferred.reject();
+      describe( 'does not include current role, has no callbacks', () => {
+        let signIn, cancel, change, $rootScope;
+        beforeEach( inject( function ( _$rootScope_ ) {
+          $rootScope = _$rootScope_;
+          signIn = jasmine.createSpy( 'success' );
+          cancel = jasmine.createSpy( 'failure' );
+          change = jasmine.createSpy( 'change' );
+          sessionEnforcerService( [Roles.registered], {}, EnforcerModes.session );
+        } ) );
+
+        it( 'opens sign-in modal and does not call \'change\' callback', () => {
+          expect( change ).not.toHaveBeenCalled();
+          expect( sessionModalService.open ).toHaveBeenCalledWith( 'sign-in', {backdrop: 'static', keyboard: false} );
+        } );
+
+        describe( 'sign-in success', () => {
+          it( 'does not call \'sign-in\' callback', () => {
+            deferred.resolve();
+            $rootScope.$digest();
+            expect( signIn ).not.toHaveBeenCalled();
+          } );
+        } );
+
+        describe( 'sign-in canceled', () => {
+          it( 'does not call \'cancel\' callback', () => {
+            deferred.reject();
+            $rootScope.$digest();
+            expect( cancel ).not.toHaveBeenCalled();
+          } );
+        } );
+      } );
+
+      describe( 'includes current role', () => {
+        let signIn, cancel, change, $rootScope;
+        beforeEach( inject( function ( _$rootScope_ ) {
+          $rootScope = _$rootScope_;
+          signIn = jasmine.createSpy( 'success' );
+          cancel = jasmine.createSpy( 'failure' );
+          change = jasmine.createSpy( 'change' );
+          sessionService.getRole.and.returnValue( Roles.registered );
+        } ) );
+
+        describe( 'callbackOnInit = true', () => {
+          it( 'does not open sign-in modal and calls \'sign-in\' callback', () => {
+            sessionEnforcerService( [Roles.registered], {
+              [EnforcerCallbacks.signIn]: signIn,
+              [EnforcerCallbacks.cancel]: cancel,
+              [EnforcerCallbacks.change]: change
+            }, 'blah', true );
+            $rootScope.$digest();
+            expect( signIn ).toHaveBeenCalled();
+            expect( sessionModalService.open ).not.toHaveBeenCalled();
+          } );
+        } );
+
+        describe( 'callbackOnInit = false', () => {
+          it( 'does not open sign-in modal and does not call \'sign-in\' callback', () => {
+            sessionEnforcerService( [Roles.registered], {
+              [EnforcerCallbacks.signIn]: signIn,
+              [EnforcerCallbacks.cancel]: cancel,
+              [EnforcerCallbacks.change]: change
+            }, 'blah', false );
+            $rootScope.$digest();
+            expect( signIn ).not.toHaveBeenCalled();
+            expect( sessionModalService.open ).not.toHaveBeenCalled();
+          } );
+        } );
+      } );
+    } );
+
+    describe( '\'donor\' mode', () => {
+      let signIn, cancel, change, $rootScope, orderService, registerAccountPromise;
+      beforeEach( inject( function ( _$rootScope_, _orderService_, _$q_ ) {
+        $rootScope = _$rootScope_;
+        orderService = _orderService_;
+        signIn = jasmine.createSpy( 'success' );
+        cancel = jasmine.createSpy( 'failure' );
+        change = jasmine.createSpy( 'change' );
+        spyOn( orderService, 'getDonorDetails' ).and.callFake( () => Observable.of( {'registration-state': 'NEW'} ) );
+        registerAccountPromise = _$q_.defer();
+        registerAccountService.and.callFake( () => registerAccountPromise.promise );
+      } ) );
+
+      describe( 'does not include current role', () => {
+        it( 'opens registerAccount modal and calls \'change\' callback', () => {
+          sessionEnforcerService( [Roles.registered], {
+            [EnforcerCallbacks.signIn]: signIn,
+            [EnforcerCallbacks.cancel]: cancel,
+            [EnforcerCallbacks.change]: change
+          }, EnforcerModes.donor );
+
+          expect( change ).toHaveBeenCalledWith( Roles.public );
+          expect( registerAccountService ).toHaveBeenCalled();
+        } );
+      } );
+
+      describe( '\'REGISTERED\' role with \'NEW\' registration-state', () => {
+        beforeEach( () => {
+          sessionService.getRole.and.returnValue( Roles.registered );
+        } );
+
+        describe( 'registerAccount', () => {
+          beforeEach( () => {
+            sessionEnforcerService( [Roles.registered], {
+              [EnforcerCallbacks.signIn]: signIn,
+              [EnforcerCallbacks.cancel]: cancel,
+              [EnforcerCallbacks.change]: change
+            }, EnforcerModes.donor );
+          } );
+
+          it( 'opens registerAccount modal and calls \'change\' callback', () => {
+            $rootScope.$digest();
+            expect( orderService.getDonorDetails ).toHaveBeenCalled();
+            expect( change ).toHaveBeenCalledWith( Roles.registered );
+            expect( registerAccountService ).toHaveBeenCalled();
+          } );
+
+          describe( 'register account success', () => {
+            it( 'calls \'sign-in\' callback', () => {
+              registerAccountPromise.resolve();
+              $rootScope.$digest();
+              expect( signIn ).toHaveBeenCalled();
+            } );
+          } );
+
+          describe( 'register account canceled', () => {
+            it( 'calls \'cancel\' callback', () => {
+              registerAccountPromise.reject();
+              $rootScope.$digest();
+              expect( cancel ).toHaveBeenCalled();
+            } );
+          } );
+        } );
+
+        describe( 'orderService.getDonorDetails() fails', () => {
+          beforeEach( () => {
+            orderService.getDonorDetails.and.callFake( () => Observable.throw( {} ) );
+          } );
+
+          it( 'opens registerAccount modal and calls \'change\' callback', () => {
+            sessionEnforcerService( [Roles.registered], {
+              [EnforcerCallbacks.signIn]: signIn,
+              [EnforcerCallbacks.cancel]: cancel,
+              [EnforcerCallbacks.change]: change
+            }, EnforcerModes.donor );
+            $rootScope.$digest();
+
+            expect( orderService.getDonorDetails ).toHaveBeenCalled();
+            expect( change ).toHaveBeenCalledWith( Roles.registered );
+            expect( registerAccountService ).toHaveBeenCalled();
+          } );
+
+          it( 'opens registerAccount modal', () => {
+            sessionEnforcerService( [Roles.registered], {
+              [EnforcerCallbacks.signIn]: signIn,
+              [EnforcerCallbacks.cancel]: cancel
+            }, EnforcerModes.donor );
+            $rootScope.$digest();
+
+            expect( orderService.getDonorDetails ).toHaveBeenCalled();
+            expect( signIn ).not.toHaveBeenCalled();
+            expect( change ).not.toHaveBeenCalled();
+            expect( cancel ).not.toHaveBeenCalled();
+            expect( registerAccountService ).toHaveBeenCalled();
+          } );
+        } );
+      } );
+
+      describe( '\'REGISTERED\' role with \'COMPLETED\' registration-state', () => {
+        beforeEach( () => {
+          sessionService.getRole.and.returnValue( Roles.registered );
+          orderService.getDonorDetails.and.callFake( () => Observable.of( {'registration-state': 'COMPLETED'} ) );
+        } );
+
+        it( 'does not open registerAccount modal and calls \'sign-in\' callback', () => {
+          sessionEnforcerService( [Roles.registered], {
+            [EnforcerCallbacks.signIn]: signIn,
+            [EnforcerCallbacks.cancel]: cancel,
+            [EnforcerCallbacks.change]: change
+          }, EnforcerModes.donor );
           $rootScope.$digest();
-          expect( cancel ).toHaveBeenCalled();
+
+          expect( orderService.getDonorDetails ).toHaveBeenCalled();
+          expect( signIn ).toHaveBeenCalled();
+          expect( change ).not.toHaveBeenCalled();
+          expect( registerAccountService ).not.toHaveBeenCalled();
+        } );
+
+        it( 'does not open registerAccount modal', () => {
+          sessionEnforcerService( [Roles.registered], {}, EnforcerModes.donor );
+          $rootScope.$digest();
+
+          expect( orderService.getDonorDetails ).toHaveBeenCalled();
+          expect( signIn ).not.toHaveBeenCalled();
+          expect( change ).not.toHaveBeenCalled();
+          expect( registerAccountService ).not.toHaveBeenCalled();
         } );
       } );
     } );
