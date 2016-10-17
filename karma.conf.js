1be0e1254397755ff8a3d7bb8d631fb3f4661003
Denys Fedotov <denlight@gmail.com>
2016-10-17 10:01:37 -0600

    removed Chrome from karma config

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome','PhantomJS'],
+    browsers: ['PhantomJS'],
     browserNoActivityTimeout: 30000,
 
     // test results reporter to use
