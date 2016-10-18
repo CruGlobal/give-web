954d1d800b02544ff01254f62dc67878fa966cbd
Denys Fedotov <denlight@gmail.com>
2016-10-18 13:03:35 -0600

    more code

--- src/app/profile/profile.component.js
+++ src/app/profile/profile.component.js
@@ -6,15 +6,18 @@ import profileService from 'common/services/api/profile.service';
 import sessionEnforcerService, {EnforcerCallbacks, EnforcerModes} from 'common/services/session/sessionEnforcer.service';
 import {Roles} from 'common/services/session/session.service';
 
+import orderService from 'common/services/api/order.service';
+
 let componentName = 'profile';
 
 class ProfileController{
 
-  constructor( $window, $location, sessionEnforcerService, profileService ) {
+  constructor( $window, $location, sessionEnforcerService, profileService, orderService ) {
     this.$window = $window;
     this.$location = $location;
     this.sessionEnforcerService = sessionEnforcerService;
     this.profileService = profileService;
+    this.orderService = orderService;
   }
 
   $onInit() {
@@ -22,12 +25,21 @@ class ProfileController{
     this.enforcerId = this.sessionEnforcerService([Roles.registered], {
       [EnforcerCallbacks.signIn]: () => {
         // Authentication success
+        this.loadDonorDetails();
       },
       [EnforcerCallbacks.cancel]: () => {
         // Authentication failure
         this.$window.location = '/';
       }
     }, EnforcerModes.donor);
+    this.loadDonorDetails();
+  }
+
+  loadDonorDetails(){
+    this.orderService.getDonorDetails()
+      .subscribe((data) => {
+        this.mailingAddress = data.mailingAddress;
+      });
   }
 }
 
