414d764897261dab648ad84aeea4db469bd64e21
adammeyer <adam.meyer@cru.org>
2016-10-20 10:45:09 -0400

    featurePath absolute path

--- src/common/components/nav/nav.component.js
+++ src/common/components/nav/nav.component.js
@@ -108,7 +108,7 @@ class NavController{
 
         let jsonStructure = angular.fromJson(response.data.jsonStructure);
         let menuStructure = {
-          main: replacePathDeep(jsonStructure['/content/cru/us/en'], {path: 'https://www.cru.org'}),
+          main: replacePathDeep(jsonStructure['/content/cru/us/en'], {path: 'https://www.cru.org', featuredPath: 'https://www.cru.org'}),
           global: jsonStructure['/content/cru/us/en/global']
         };
 
