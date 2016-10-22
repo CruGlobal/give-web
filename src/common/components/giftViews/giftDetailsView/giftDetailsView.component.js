e4bfb8e14b83d8366cf336d566ed7d3b29c12427
Scotty Waggoner <ozzieorca@gmail.com>
2016-10-22 14:25:41 -0700

    Implement edit recurring gifts confirmation page (step 4)
    - Pass all recurring gifts between components and let step 4 filter them out
    - Have step 1 use cached (and possibly modified) gifts instead of reloading them
    - Rename giftSimpleView to giftSummaryView
    - Add giftDetailsView and giftUpdateView
    - Add recurringGift model to handle reading, updating, and manipulating dates on a gift object
    - Change updateRecurringGifts to send gifts in donation-lines key

---
+++ src/common/components/giftViews/giftDetailsView/giftDetailsView.component.js
@@ -0,0 +1,31 @@
+import angular from 'angular';
+import 'angular-ordinal';
+
+import paymentMethodDisplay from 'common/components/paymentMethods/paymentMethodDisplay.component';
+import {quarterlyMonths} from 'common/services/giftHelpers/giftDates.service';
+
+import template from './giftDetailsView.tpl';
+
+let componentName = 'giftDetailsView';
+
+class GiftDetailsViewController {
+
+  /* @ngInject */
+  constructor() {
+    this.quarterlyMonths = quarterlyMonths;
+  }
+}
+
+export default angular
+  .module( componentName, [
+    template.name,
+    paymentMethodDisplay.name,
+    'ordinal'
+  ] )
+  .component( componentName, {
+    controller:  GiftDetailsViewController,
+    templateUrl: template.name,
+    bindings:    {
+      gift: '<'
+    }
+  } );
