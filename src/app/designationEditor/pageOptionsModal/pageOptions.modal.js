983fed15af052b2e0f4cbafed90607f1d18b37f9
adammeyer <adam.meyer@cru.org>
2016-11-03 14:33:49 -0400

    page options: suggested amounts

--- src/app/designationEditor/pageOptionsModal/pageOptions.modal.js
+++ src/app/designationEditor/pageOptionsModal/pageOptions.modal.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import transform from 'lodash/transform';
 
 let controllerName = 'pageOptionsCtrl';
 
@@ -8,7 +9,20 @@ class ModalInstanceCtrl {
   constructor( parentDesignationNumber, organizationId, suggestedAmounts ) {
     this.parentDesignationNumber = parentDesignationNumber;
     this.organizationId = organizationId;
-    this.suggestedAmounts = suggestedAmounts;
+
+    this.suggestedAmounts = transform(suggestedAmounts, function(result, value, key) {
+      if(key === 'jcr:primaryType'){ return; }
+      result.push({
+        amount: Number(key),
+        description: value
+      });
+    }, []);
+  }
+
+  transformSuggestedAmounts(){
+    return transform(this.suggestedAmounts, function(result, n) {
+      result[n.amount] = n.description;
+    }, {});
   }
 }
 
