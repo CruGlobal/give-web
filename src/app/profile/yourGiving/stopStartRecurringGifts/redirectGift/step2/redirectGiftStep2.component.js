4334bbbec765623c839ca437b03da931e7a638b5
Scotty Waggoner <ozzieorca@gmail.com>
2016-11-16 15:44:51 -0800

    Create common giftSearchView component and use it in redirectGift modal
    - Fix spelling and styling in redirectGift modal

--- src/app/profile/yourGiving/stopStartRecurringGifts/redirectGift/step2/redirectGiftStep2.component.js
+++ src/app/profile/yourGiving/stopStartRecurringGifts/redirectGift/step2/redirectGiftStep2.component.js
@@ -1,72 +1,26 @@
 import angular from 'angular';
 import template from './redirectGiftStep2.tpl';
-import find from 'lodash/find';
-import debounce from 'lodash/debounce';
-import map from 'lodash/map';
 
-import designationsService from 'common/services/api/designations.service';
-
-import giftListItem from 'common/components/giftViews/giftListItem/giftListItem.component';
+import giftSearchView from 'common/components/giftViews/giftSearchView/giftSearchView.component';
 
 let componentName = 'redirectGiftStep2';
 
 class RedirectGiftStep2Controller {
 
   /* @ngInject */
-  constructor( designationsService ) {
-    this.designationsService = designationsService;
-    this.find = find;
+  constructor() {
 
-    // Create debounced search method
-    this.performSearch = debounce( this._performSearch, 750 );
   }
 
-  doSearch() {
-    if ( angular.isDefined( this.subscriber ) ) {
-      this.subscriber.unsubscribe();
-      this.subscriber = undefined;
-    }
-    this.searchState = 'searching';
-    this.performSearch();
-  }
-
-  /**
-   * @private
-   */
-  _performSearch() {
-    this.subscriber = this.designationsService.productSearch( {keyword: this.search} ).subscribe( ( results ) => {
-      if ( results.length ) {
-        this.results = map( results, ( result ) => {
-          return {designationNumber: result.designationNumber, designationName: result.name};
-        } );
-        this.searchState = 'results';
-      }
-      else {
-        this.searchState = 'no-results';
-      }
-      this.subscriber = undefined;
-    } );
-  }
-
-  selectResult() {
-    this.onSelectResult( {selected: find( this.results, {_selectedGift: true} )} );
-  }
-
-  resultSelected( result ) {
-    // required for selectable="radio", deselect previous results
-    angular.forEach( this.results, ( item ) => {
-      if ( result !== item ) {
-        item._selectedGift = false;
-      }
-    } );
+  onSelection(selectedRecipient) {
+    this.selected = selectedRecipient;
   }
 }
 
 export default angular
   .module( componentName, [
     template.name,
-    designationsService.name,
-    giftListItem.name
+    giftSearchView.name
   ] )
   .component( componentName, {
       controller:  RedirectGiftStep2Controller,
