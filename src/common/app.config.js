c03344f7ab68e2202482aced358970616869c5a5
Adam Meyer <adam.meyer@cru.org>
2016-11-18 13:26:53 -0500

    Update app.config.js

--- src/common/app.config.js
+++ src/common/app.config.js
@@ -14,7 +14,7 @@ function appConfig(envServiceProvider, $compileProvider, $logProvider, $httpProv
   envServiceProvider.config({
     domains: {
       development: ['localhost', 'localhost.cru.org'],
-      staging: ['give-stage2.cru.org', 'stage.cru.org', 'devauth.aws.cru.org', 'devpub.aws.cru.org', 'uatauth.aws.cru.org', 'uatpub.aws.cru.org'],
+      staging: ['give-stage2.cru.org', 'stage.cru.org', 'dev.aws.cru.org', 'devauth.aws.cru.org', 'devpub.aws.cru.org', 'uatauth.aws.cru.org', 'uatpub.aws.cru.org'],
       production: ['www.cru.org', 'give.cru.org', 'author.cru.org']
     },
     vars: {
