2ffa39a27d5fc3e05419cf30fee1a04543ec1cba
Denys Fedotov <denlight@gmail.com>
2016-11-21 08:29:16 -0700

    added cart button and update checkout btn behavior (#283)

--- src/common/components/nav/nav.component.js
+++ src/common/components/nav/nav.component.js
@@ -217,6 +217,10 @@ class NavController{
     });
   }
 
+  checkout() {
+    this.$window.location = this.sessionService.getRole() === 'REGISTERED' ? '/checkout.html' : '/sign-in.html';
+  }
+
 }
 
 export default angular
