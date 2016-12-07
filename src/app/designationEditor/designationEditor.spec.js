bc5028ccbe07537243b4d9092d5c50734cafdfb9
adammeyer <adam.meyer@cru.org>
2016-12-07 16:24:31 -0500

    designation editor fixes, EP-1566, EP-1567, EP-1570

--- src/app/designationEditor/designationEditor.spec.js
+++ src/app/designationEditor/designationEditor.spec.js
@@ -172,6 +172,7 @@ describe( 'Designation Editor', function () {
 
         expect($ctrl.$uibModal.open).toHaveBeenCalled();
         expect($ctrl.$uibModal.open.calls.argsFor( 0 )[0].resolve.parentDesignationNumber()).toEqual(designationSecurityResponse.parentDesignationNumber);
+        expect($ctrl.$uibModal.open.calls.argsFor( 0 )[0].resolve.designationType()).toEqual(designationSecurityResponse.designationType);
         expect($ctrl.$uibModal.open.calls.argsFor( 0 )[0].resolve.organizationId()).toEqual(designationSecurityResponse.organizationId);
         expect($ctrl.$uibModal.open.calls.argsFor( 0 )[0].resolve.suggestedAmounts()).toEqual(designationSecurityResponse.suggestedAmounts);
 
