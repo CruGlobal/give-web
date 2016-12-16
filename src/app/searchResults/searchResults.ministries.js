dbc19ec9fa4b6719db9ce379cf35b03ebf3d5efb
Adam Meyer <adam.meyer@cru.org>
2016-12-16 10:45:06 -0500

    use hard coded ministry list if no keyword entered (#345)

--- src/app/searchResults/searchResults.ministries.js
+++ src/app/searchResults/searchResults.ministries.js
@@ -1,179 +1,223 @@
 export default [
   {
     name: 'Africa',
-    designationNumber: '2802860'
+    designationNumber: '2802860',
+    path: '/2802860'
   },
   {
     name: 'Andre Kole Ministries',
-    designationNumber: '2294586'
+    designationNumber: '2294586',
+    path: '/2294586'
   },
   {
     name: 'Asia',
-    designationNumber: '2802878'
+    designationNumber: '2802878',
+    path: '/2802878'
   },
   {
     name: 'Athletes in Action',
-    designationNumber: '2863856'
+    designationNumber: '2863856',
+    path: '/2863856'
   },
   {
     name: 'Campus Ministry',
-    designationNumber: '2752539'
+    designationNumber: '2752539',
+    path: '/2752539'
   },
   {
     name: 'Central America, South America and Caribbean',
-    designationNumber: '2802885'
+    designationNumber: '2802885',
+    path: '/2802885'
   },
   {
     name: 'City',
-    designationNumber: '2291234'
+    designationNumber: '2291234',
+    path: '/2291234'
   },
   {
     name: 'Christian Embassy - New York',
-    designationNumber: '2277989'
+    designationNumber: '2277989',
+    path: '/2277989'
   },
   {
     name: 'Christian Embassy DC',
-    designationNumber: '2277786'
+    designationNumber: '2277786',
+    path: '/2277786'
   },
   {
     name: 'Compassionate Evangelism / Responding to Needs',
-    designationNumber: '2847275'
+    designationNumber: '2847275',
+    path: '/2847275'
   },
   {
     name: 'Crossroads',
-    designationNumber: '2509124'
+    designationNumber: '2509124',
+    path: '/2509124'
   },
   {
     name: 'Cru Military',
-    designationNumber: '2283732'
+    designationNumber: '2283732',
+    path: '/2283732'
   },
   {
     name: 'Europe and Russia',
-    designationNumber: '2802853'
+    designationNumber: '2802853',
+    path: '/2802853'
   },
   {
     name: 'EveryStudent.com',
-    designationNumber: '2787935'
+    designationNumber: '2787935',
+    path: '/2787935'
   },
   {
     name: 'Faculty Commons',
-    designationNumber: '2271527'
+    designationNumber: '2271527',
+    path: '/2271527'
   },
   {
     name: 'FamilyLife',
-    designationNumber: '2294554'
+    designationNumber: '2294554',
+    path: '/2294554'
   },
   {
     name: 'Global Aid Network (GAIN)',
-    designationNumber: '2807640'
+    designationNumber: '2807640',
+    path: '/2807640'
   },
   {
     name: 'Global Ministries',
-    designationNumber: '2501316'
+    designationNumber: '2501316',
+    path: '/2501316'
   },
   {
     name: 'High School',
-    designationNumber: '2290417'
+    designationNumber: '2290417',
+    path: '/2290417'
   },
   {
     name: 'Inner City',
-    designationNumber: '2272458'
+    designationNumber: '2272458',
+    path: '/2272458'
   },
   {
     name: 'International - Australia, New Zealand, Pacific',
-    designationNumber: '2848007'
+    designationNumber: '2848007',
+    path: '/2848007'
   },
   {
     name: 'International - Central Asia and Turkey',
-    designationNumber: '2873715'
+    designationNumber: '2873715',
+    path: '/2873715'
   },
   {
     name: 'International - East Asia',
-    designationNumber: '2732102'
+    designationNumber: '2732102',
+    path: '/2732102'
   },
   {
     name: 'International - Eastern Europe and Russia',
-    designationNumber: '2582147'
+    designationNumber: '2582147',
+    path: '/2582147'
   },
   {
     name: 'International - Francophone Africa',
-    designationNumber: '2598030'
+    designationNumber: '2598030',
+    path: '/2598030'
   },
   {
     name: 'International - Latin America and the Caribbean',
-    designationNumber: '2580783'
+    designationNumber: '2580783',
+    path: '/2580783'
   },
   {
     name: 'International - Norh Africa and Middle East',
-    designationNumber: '2879337'
+    designationNumber: '2879337',
+    path: '/2879337'
   },
   {
     name: 'International - South Asia',
-    designationNumber: '2572249'
+    designationNumber: '2572249',
+    path: '/2572249'
   },
   {
     name: 'International - Southeast Asia',
-    designationNumber: '2580071'
+    designationNumber: '2580071',
+    path: '/2580071'
   },
   {
     name: 'International - Southern and Eastern Africa',
-    designationNumber: '2580018'
+    designationNumber: '2580018',
+    path: '/2580018'
   },
   {
     name: 'International - West Africa',
-    designationNumber: '2578907'
+    designationNumber: '2578907',
+    path: '/2578907'
   },
   {
     name: 'International - Western Europe',
-    designationNumber: '2580395'
+    designationNumber: '2580395',
+    path: '/2580395'
   },
   {
     name: 'ISP',
-    designationNumber: '2762704'
+    designationNumber: '2762704',
+    path: '/2762704'
   },
   {
     name: 'Jesus Film Project',
-    designationNumber: '2592320'
+    designationNumber: '2592320',
+    path: '/2592320'
   },
   {
     name: 'Josh McDowell Ministry',
-    designationNumber: '2579585'
+    designationNumber: '2579585',
+    path: '/2579585'
   },
   {
     name: 'Keynote',
-    designationNumber: '2278105'
+    designationNumber: '2278105',
+    path: '/2278105'
   },
   {
     name: 'New Life Resources',
-    designationNumber: '2777777'
+    designationNumber: '2777777',
+    path: '/2777777'
   },
   {
     name: 'President\'s Office',
-    designationNumber: '2847001'
+    designationNumber: '2847001',
+    path: '/2847001'
   },
   {
     name: 'Special Ministries',
-    designationNumber: '2835390'
+    designationNumber: '2835390',
+    path: '/2835390'
   },
   {
     name: 'StoryRunners',
-    designationNumber: '2852450'
+    designationNumber: '2852450',
+    path: '/2852450'
   },
   {
     name: 'US Development Priorities',
-    designationNumber: '2764993'
+    designationNumber: '2764993',
+    path: '/2764993'
   },
   {
     name: 'World Headquarters at Lake Hart',
-    designationNumber: '2732586'
+    designationNumber: '2732586',
+    path: '/2732586'
   },
   {
     name: 'World Priorities',
-    designationNumber: '2800934'
+    designationNumber: '2800934',
+    path: '/2800934'
   },
   {
     name: 'Worldwide Challenge',
-    designationNumber: '2573504'
+    designationNumber: '2573504',
+    path: '/2573504'
   }
 ];
 
