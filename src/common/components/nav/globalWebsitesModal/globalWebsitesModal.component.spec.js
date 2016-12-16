eaa2ca5d6a4d89a99704f25f2b9ae3a1cf2a0c7c
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-12-16 15:24:45 -0500

    [#EP-1558] Added scrollspy to global navigation.

--- src/common/components/nav/globalWebsitesModal/globalWebsitesModal.component.spec.js
+++ src/common/components/nav/globalWebsitesModal/globalWebsitesModal.component.spec.js
@@ -15,21 +15,32 @@ describe('globalWebsitesModal', () => {
 
   it('should be defined', () => {
     expect(self.controller).toBeDefined();
-    expect(self.controller.$anchorScroll).toBeDefined();
-    expect(self.controller.$location).toBeDefined();
+  });
+
+  describe('$onInit()', () => {
+    it('should initialize the component', () => {
+      let element = {id: 'container'};
+      spyOn(document, 'getElementById').and.returnValue(element);
+      self.controller.$onInit();
+      expect(document.getElementById).toHaveBeenCalledWith('globalWebsites-modal');
+      expect(self.controller.container).toBeDefined();
+    });
   });
 
   describe('scrollTo()', () => {
-    it('should do anchor tag navigation', () => {
-      spyOn(self.controller, '$anchorScroll');
-      self.controller.scrollTo('ukraine');
-      expect(self.controller.country).toBe('ukraine');
+    it('should call container.scrollTo()', () => {
+      let element = {id: 'globalWebsites-continent--africa'};
+      spyOn(document, 'getElementById').and.returnValue(element);
+      self.controller.container = jasmine.createSpyObj('container', ['scrollTo']);
+      self.controller.scrollTo('globalWebsites-continent--africa');
+      expect(document.getElementById).toHaveBeenCalledWith('globalWebsites-continent--africa');
+      expect(self.controller.container.scrollTo).toHaveBeenCalledWith(jasmine.anything(), 0, 300);
     });
   });
 
   describe('getContinentId()', () => {
-    it('should return clean id', () => {
-      expect(self.controller.getContinentId('/ukraine')).toBe('ukraine');
+    it('should return clean div id', () => {
+      expect(self.controller.getContinentId('/africa')).toBe('globalWebsites-continent--africa');
     });
   });
 });
