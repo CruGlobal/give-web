57ac69d431960bce75166651ca41d8eee8397745
Adam Meyer <adam.meyer@cru.org>
2017-01-09 09:06:52 -0500

    use photo upload response (#377)
    * use photo upload response
    
    * lint
    
    * ministries and campaigns can edit title

--- src/app/designationEditor/photoModal/photoModal.spec.js
+++ src/app/designationEditor/photoModal/photoModal.spec.js
@@ -4,13 +4,10 @@ import module from './photo.modal';
 
 describe('Designation Editor Photo', function() {
   beforeEach(angular.mock.module(module.name));
-  var $rootScope, $ctrl, $q, $timeout;
+  var $ctrl;
 
-  beforeEach(inject(function(_$rootScope_, _$controller_, _$q_, _$timeout_) {
+  beforeEach(inject(function(_$rootScope_, _$controller_) {
     var $scope = _$rootScope_.$new();
-    $rootScope = _$rootScope_;
-    $timeout = _$timeout_;
-    $q = _$q_;
 
     $ctrl = _$controller_( module.name, {
       designationNumber: '000555',
@@ -34,16 +31,8 @@ describe('Designation Editor Photo', function() {
   });
 
   it('uploadComplete', function() {
-    let getPhotosPromise = $q.defer();
-    spyOn( $ctrl.designationEditorService, 'getPhotos' ).and.returnValue(  getPhotosPromise.promise );
 
-    $ctrl.uploadComplete();
-    $timeout.flush();
-
-    getPhotosPromise.resolve({data: []});
-    $rootScope.$digest();
-
-    expect($ctrl.designationEditorService.getPhotos).toHaveBeenCalled();
+    $ctrl.uploadComplete({data: []});
     expect($ctrl.photos).toEqual([]);
   });
 });
