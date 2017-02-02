87fe3fa431cf07735926e01fe36116a497473e0e
adammeyer <adam.meyer@cru.org>
2017-02-02 15:06:27 -0500

    reset password on sign in page

--- src/app/signIn/signIn.spec.js
+++ src/app/signIn/signIn.spec.js
@@ -86,4 +86,17 @@ describe( 'signIn', function () {
       } );
     } );
   } );
+
+  describe( 'resetPassword()', () => {
+    let deferred;
+    beforeEach( inject( function ( _$q_ ) {
+      deferred = _$q_.defer();
+      spyOn( $ctrl.sessionModalService, 'forgotPassword' ).and.callFake( () => deferred.promise );
+      $ctrl.resetPassword();
+    } ) );
+
+    it( 'opens reset password modal', () => {
+      expect( $ctrl.sessionModalService.forgotPassword ).toHaveBeenCalled();
+    } );
+  } );
 } );
