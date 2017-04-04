417e1e82f81570c7793e8efb7b95fbae161199cb
Scotty Waggoner <ozzieorca@gmail.com>
2017-04-03 20:06:43 -0700

    In contactInfo component, disable spouse fields for staff

--- src/common/components/contactInfo/contactInfo.component.js
+++ src/common/components/contactInfo/contactInfo.component.js
@@ -68,7 +68,7 @@ class Step1Controller{
         this.loadingDonorDetails = false;
         this.donorDetails = data;
         this.nameFieldsDisabled = this.donorDetails['registration-state'] === 'COMPLETED';
-        this.spouseFieldsDisabled = !!this.donorDetails['spouse-name']['given-name'] || !!this.donorDetails['spouse-name']['family-name'];
+        this.spouseFieldsDisabled = !!this.donorDetails['spouse-name']['given-name'] || !!this.donorDetails['spouse-name']['family-name'] || this.donorDetails.staff;
         if(!this.nameFieldsDisabled && includes([Roles.registered, Roles.identified], this.sessionService.getRole())) {
           // Pre-populate first, last and email from session if missing from donorDetails
           if(!this.donorDetails['name']['given-name'] && angular.isDefined(this.sessionService.session.first_name)) {
