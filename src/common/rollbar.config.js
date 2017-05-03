a184d3c5913ab71647ee11539bff326138a77174
Scotty Waggoner <ozzieorca@gmail.com>
2017-05-03 13:21:44 -0700

    Scrub password reset k query param from rollbar

--- src/common/rollbar.config.js
+++ src/common/rollbar.config.js
@@ -17,7 +17,7 @@ function rollbarConfig(envServiceProvider, $provide) {
     enabled: !envServiceProvider.is('development'), // Disable rollbar in development environment
     transform: transformRollbarPayload,
     hostWhiteList: ['give.cru.org', 'give-prod.cru.org', 'give-stage2.cru.org', 'dev.aws.cru.org', 'devauth.aws.cru.org', 'devpub.aws.cru.org', 'uatauth.aws.cru.org', 'uatpub.aws.cru.org'],
-    scrubFields: ['password', 'cvv', 'cvv2', 'security-code'],
+    scrubFields: ['password', 'cvv', 'cvv2', 'security-code', 'k'],
     payload: {
       client: {
         javascript: {
