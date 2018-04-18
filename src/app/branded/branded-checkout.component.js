3ac4307fc302f1e0214f268a989696f99f9b4593
Adam <adam.meyer@cru.org>
2018-04-17 22:02:00 -0400

    remove branded checkout logout

--- src/app/branded/branded-checkout.component.js
+++ src/app/branded/branded-checkout.component.js
@@ -32,10 +32,8 @@ class BrandedCheckoutController {
     this.analyticsFactory.pageLoaded(true);
     this.formatDonorDetails();
 
-    this.sessionService.signOut().subscribe(() => {
-      this.checkoutStep = 'giftContactPayment';
-      this.fireAnalyticsEvents('contact', 'payment');
-    }, angular.noop);
+    this.checkoutStep = 'giftContactPayment';
+    this.fireAnalyticsEvents('contact', 'payment');
   }
 
   formatDonorDetails(){
