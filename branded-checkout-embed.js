8cdad429ed71c9d8830b203049f1715e1e31a5eb
Adam <adam.meyer@cru.org>
2018-04-18 11:23:06 -0400

    branded checkout iframe

---
+++ branded-checkout-embed.js
@@ -0,0 +1,31 @@
+(function(){
+  window.onload = function () {
+    var brandedElement = document.getElementsByTagName('branded-checkout')[0];
+    if(!brandedElement){
+      console.log('No branded checkout element found.');
+      return;
+    }
+
+    var attributes = {};
+    [].slice.call(brandedElement.attributes).forEach(function(attr){
+      attributes[attr.nodeName] = attr.nodeValue;
+    });
+
+    var iframe = document.createElement('iframe');
+    iframe.style.width = '100%';
+    iframe.style.border = 'none';
+    iframe.style.overflow = 'hidden';
+    iframe.setAttribute('allowtransparency', 'true');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.src = 'https://give-static.cru.org/brandedCheckoutEmbed.html#' + btoa(JSON.stringify(attributes));
+    brandedElement.appendChild(iframe);
+
+    var eventMethod = window.addEventListener ? 'addEventListener' : 'attachEvent';
+    var eventer = window[eventMethod];
+    var messageEvent = eventMethod === 'attachEvent' ? 'onmessage' : 'message';
+    eventer(messageEvent, function(e) {
+      if (isNaN(e.data)) return;
+      iframe.style.height = e.data + 'px';
+    }, false);
+  };
+})();
