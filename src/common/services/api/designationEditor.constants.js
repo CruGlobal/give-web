2aea78e81d420c9f45baa60d27f78c2b1ec28959
Brian Zoetewey <brian.zoetewey@cru.org>
2019-08-28 16:03:36 -0400

    Update newsletter signup endpoints

--- src/common/services/api/designationEditor.constants.js
+++ src/common/services/api/designationEditor.constants.js
@@ -1,6 +1,6 @@
 export default {
   designationEndpoint: '/bin/crugive/designation',
   designationImagesEndpoint: '/bin/crugive/image',
-  designationNewsletter: '/bin/crugive/newsletter/distributor',
-  designationNewsletterSubscription: '/bin/crugive/newsletter/subscription'
+  designationNewsletter: '/bin/crugive/newsletter/signup.json',
+  designationNewsletterSubscription: '/bin/crugive/newsletter/signup.json'
 }
