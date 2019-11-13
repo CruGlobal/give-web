f60018e9300345b372d2fdf8a5bebcf1ec984ebb
Bill Randall <william.randall@cru.org>
2019-11-13 17:22:25 -0500

    Prevent adding too many photos to the carousel

--- src/app/designationEditor/photoModal/photo.modal.js
+++ src/app/designationEditor/photoModal/photo.modal.js
@@ -16,6 +16,8 @@ class ModalInstanceCtrl {
     this.photoLocation = photoLocation
     this.selectedPhoto = selectedPhoto
     this.photos = photos
+
+    this.maxNumberOfPhotos = 6
   }
 
   uploadComplete () {
@@ -29,12 +31,19 @@ class ModalInstanceCtrl {
   }
 
   addImageToCarousel (photo) {
-    this.selectedPhoto = [...this.selectedPhoto, { url: photo.original }]
+    if (!this.selectedPhoto || (this.selectedPhoto && this.selectedPhoto.length < this.maxNumberOfPhotos)) {
+      this.maxCarouselError = false
+      this.selectedPhoto = [...this.selectedPhoto, { url: photo.original }]
+    } else {
+      // Send an error telling the user that they can only have this.maxNumberOfPhotos photos in the carousel
+      this.maxCarouselError = true
+    }
   }
 
   reorderImageInCarousel (index, newIndex) {
     const item = this.selectedPhoto.splice(index, 1)
     this.selectedPhoto.splice(newIndex, 0, ...item)
+    this.maxCarouselError = false
   }
 }
 
