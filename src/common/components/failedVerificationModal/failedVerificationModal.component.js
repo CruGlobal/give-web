e598ad389af0401ef5f9e926e28beca6e8c32a19
Bill Randall <william.randall@cru.org>
2020-02-26 15:02:37 -0500

    Remove unused controller

--- src/common/components/failedVerificationModal/failedVerificationModal.component.js
+++ src/common/components/failedVerificationModal/failedVerificationModal.component.js
@@ -3,15 +3,9 @@ import template from './failedVerificationModal.tpl.html'
 
 const componentName = 'failedVerificationModal'
 
-class FailedVerificationModalController {
-  /* @ngInject */
-  constructor () /* eslint-disable-line no-useless-constructor */ {}
-}
-
 export default angular
   .module(componentName, [])
   .component(componentName, {
-    controller: FailedVerificationModalController,
     templateUrl: template,
     bindings: {
       title: '@',
