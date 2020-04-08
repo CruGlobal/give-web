d71ecc648b7933582c876268eebac1c6a0ea32ab
Bill Randall <william.randall@cru.org>
2020-04-08 16:54:20 -0400

    Add more support for typescript (the tsconfig was basically copied from missionhub-react-native)

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,8 @@ module.exports = {
         }
       }
     ],
-    '@babel/preset-react'
+    '@babel/preset-react',
+    '@babel/preset-typescript'
   ],
   plugins: [
     '@babel/plugin-transform-runtime'
