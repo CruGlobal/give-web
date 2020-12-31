6b0855fd66494b196db8b4df4723ff991650aa72
Bill Randall <william.randall@cru.org>
2020-12-31 16:40:30 -0500

    Do not try to turn date strings to dates if we have no items object

--- src/common/services/api/order.service.spec.js
+++ src/common/services/api/order.service.spec.js
@@ -903,6 +903,18 @@ describe('order service', () => {
       self.$window.localStorage.setItem('cartData', angular.toJson(cartData))
       expect(self.orderService.retrieveCartData()).toEqual(cartData)
     })
+
+    it('should not turn the gift start date into a moment if the cart does not exist', () => {
+      jest.spyOn(self.orderService, 'turnDateStringsToDates')
+      self.$window.localStorage.setItem('cartData', null)
+      expect(self.orderService.turnDateStringsToDates).not.toHaveBeenCalled()
+    })
+
+    it('should not turn the gift start date into a moment if the cart does not have items', () => {
+      jest.spyOn(self.orderService, 'turnDateStringsToDates')
+      self.$window.localStorage.setItem('cartData', '{}')
+      expect(self.orderService.turnDateStringsToDates).not.toHaveBeenCalled()
+    })
   })
 
   describe('addItemToCartData', () => {
