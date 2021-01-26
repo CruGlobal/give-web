3f1c7f4dddf5624ee94195376491747903b3bad8
Bill Randall <william.randall@cru.org>
2021-01-26 09:28:24 -0500

    Calculate the price to show to users with the checkbox for branded checkout. The changes in step-1 are for situations where the amount comes in with the branded-checkout directive. The changes in coverFees are for situations where the amount did not come in with the directive.

--- src/app/branded/step-1/branded-checkout-step-1.component.js
+++ src/app/branded/step-1/branded-checkout-step-1.component.js
@@ -8,14 +8,17 @@ import checkoutStep2 from 'app/checkout/step-2/step-2.component'
 import cartService from 'common/services/api/cart.service'
 import orderService from 'common/services/api/order.service'
 
+import { FEE_DERIVATIVE } from 'common/components/paymentMethods/coverFees/coverFees.component'
+
 import template from './branded-checkout-step-1.tpl.html'
 
 const componentName = 'brandedCheckoutStep1'
 
 class BrandedCheckoutStep1Controller {
   /* @ngInject */
-  constructor ($log, cartService, orderService) {
+  constructor ($log, $filter, cartService, orderService) {
     this.$log = $log
+    this.$filter = $filter
     this.cartService = cartService
     this.orderService = orderService
   }
@@ -35,6 +38,13 @@ class BrandedCheckoutStep1Controller {
     }
     this.itemConfig['campaign-page'] = this.campaignPage
     this.itemConfig.amount = this.amount
+
+    // These lines calculate the price with fees for amounts coming in from the client site via component config
+    if (this.amount) {
+      const amountWithFees = this.amount / FEE_DERIVATIVE
+      this.itemConfig.priceWithFees = this.$filter('currency')(amountWithFees, '$', 2)
+    }
+
     switch (this.frequency) {
       case 'monthly':
         this.defaultFrequency = 'MON'
