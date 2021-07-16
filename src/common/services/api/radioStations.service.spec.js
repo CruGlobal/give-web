a7d920254dfbc86abed3a811c96048ba5c86f659
Robert Eldredge <robert.eldredge@cru.org>
2021-07-16 15:49:20 -0400

    fixed the test

--- src/common/services/api/radioStations.service.spec.js
+++ src/common/services/api/radioStations.service.spec.js
@@ -7,9 +7,9 @@ describe('radio station service', () => {
   beforeEach(angular.mock.module(module.name))
   var self = {}
 
-  beforeEach(inject((_radioStationService_, _$httpBackend_) => {
-    self.radioStationService = _radioStationService_
-    self.$httpBackend = _$httpBackend_
+  beforeEach(inject((radioStationsService, $httpBackend) => {
+    self.radioStationsService = radioStationsService
+    self.$httpBackend = $httpBackend
   }))
 
   afterEach(() => {
@@ -21,7 +21,7 @@ describe('radio station service', () => {
     it('returns list of radio stations', () => {
       const expectedRadioStations = [{ Description: 'Radio Station', MediaId: 'WXYZ' }, { Description: 'Other Station', MediaId: 'ZYXW' }]
   
-      self.$httpBackend.expectGET('https://api.domain.com/getStations/333333/100')
+      self.$httpBackend.expectGET('https://api.domain.com/getStations/33333/100')
         .respond(200, expectedRadioStations)
       self.radioStationsService.getRadioStations('https://api.domain.com/getStations', '33333', '100')
         .subscribe((data) => {
