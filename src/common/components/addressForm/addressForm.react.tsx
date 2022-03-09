72d2e73851a3d574f06d8d08cfab118638ec8143
Robert Eldredge <robert.eldredge@cru.org>
2022-03-09 11:28:58 -0500

    Update AddressForm Components to Work With Formik

--- src/common/components/addressForm/addressForm.react.tsx
+++ src/common/components/addressForm/addressForm.react.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import angular from 'angular';
 import { react2angular } from 'react2angular';
+import { Formik, FormikErrors } from 'formik';
 import find from 'lodash/find';
 
 import CountrySelect from './countrySelect';
@@ -64,6 +65,47 @@ const AddressForm = ({
     return 0;
   };
 
+  const validator = (values: Address): FormikErrors<Address> => {
+    const errors: FormikErrors<Address> = {};
+
+    if (loadingCountriesError) {
+      errors.country = 'COUNTRY_LIST_ERROR';
+    } else if (!values.country) {
+      errors.country = 'COUNTRY_SELECT_ERROR';
+    }
+    if (!values.streetAddress) {
+      errors.streetAddress = 'ADDRESS_ERROR';
+    } else if (values.streetAddress.length > 200) {
+      errors.streetAddress = 'MAX_LENGTH_ADDRESS_ERROR';
+    }
+    if (values.extendedAddress && values.extendedAddress.length > 100) {
+      errors.extendedAddress = 'MAX_LENGTH_ADDRESS_OTHERS_ERROR';
+    }
+    if (values.intAddressLine3 && values.intAddressLine3.length > 100) {
+      errors.intAddressLine3 = 'MAX_LENGTH_ADDRESS_OTHERS_ERROR';
+    }
+    if (values.intAddressLine4 && values.intAddressLine4.length > 100) {
+      errors.intAddressLine4 = 'MAX_LENGTH_ADDRESS_OTHERS_ERROR';
+    }
+    if (!values.locality) {
+      errors.locality = 'CITY_ERROR';
+    } else if (values.locality.length > 50) {
+      errors.locality = 'MAX_LENGTH_CITY_ERROR';
+    }
+    if (loadingRegionsError) {
+      errors.country = 'REGIONS_LOADING_ERROR';
+    } else if (!values.region) {
+      errors.region = 'SELECT_STATE_ERROR';
+    }
+    if (!values.postalCode) {
+      errors.postalCode = 'ZIP_CODE_ERROR';
+    } else if (!/^\d{5}(?:[-\s]\d{4})?$/.test(values.postalCode)) {
+      errors.postalCode = 'INVALID_US_ZIP_ERROR';
+    }
+
+    return errors;
+  };
+
   const loadCountries = () => {
     setLoadingCountriesError(false);
 
@@ -113,128 +155,142 @@ const AddressForm = ({
   }
 
   return (
-    <>
-      <div className="row">
-        <div className="col-sm-12">
-          <CountrySelect
-            addressDisabled={addressDisabled}
-            countries={countries.map(country => ({ name: country.name, displayName: country['display-name']}))}
-            onChange={}
-            onBlur={}
-            onSelectCountry={setCountryName}
-            refreshCountries={loadCountries}
-            value={}
-            error={}
-            canRetry={}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-12">
-          <TextInput
-            title="Address"
-            name="streetAddress"
-            required
-            maxLength={200}
-            disabled={addressDisabled}
-            onChange={}
-            onBlur={}
-            value={}
-            error={}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-12">
-          <TextInput
-            name="extendedAddress"
-            maxLength={100}
-            disabled={addressDisabled}
-            onChange={}
-            onBlur={}
-            value={}
-            error={}
-          />
-        </div>
-      </div>
-      {
-        countryName && countryName !== 'US'
-        ? (
-          <div>
-            <div className="row">
-              <div className="col-sm-12">
-                <TextInput
-                  name="intAddressLine3"
-                  maxLength={100}
-                  disabled={addressDisabled}
-                  onChange={}
-                  onBlur={}
-                  value={}
-                  error={}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-12">
-                <TextInput
-                  name="intAddressLine4"
-                  maxLength={100}
-                  disabled={addressDisabled}
-                  onChange={}
-                  onBlur={}
-                  value={}
-                  error={}
-                />
-              </div>
+    <Formik
+      initialValues={address}
+      validate={validator}
+      onSubmit={}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+      }) => (
+        <>
+          <div className="row">
+            <div className="col-sm-12">
+              <CountrySelect
+                addressDisabled={addressDisabled}
+                countries={countries.map(country => ({ name: country.name, displayName: country['display-name']}))}             
+                onChange={handleChange}
+                onBlur={handleBlur}
+                onSelectCountry={setCountryName}
+                refreshCountries={loadCountries}
+                value={values.country}
+                error={touched.country && errors.country || undefined}
+                canRetry={loadingCountriesError}
+              />
             </div>
           </div>
-        ) : (
-          <div>
-            <div className="row">
-              <div className="col-sm-12">
-                <TextInput
-                  title="City"
-                  name="locality"
-                  required
-                  maxLength={50}
-                  disabled={addressDisabled}
-                  onChange={}
-                  onBlur={}
-                  value={}
-                  error={}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-6">
-                <RegionSelect
-                  addressDisabled={addressDisabled}
-                  regions={regions.map(region => ({ name: region.name, displayName: region['display-name']}))}
-                  onChange={}
-                  onBlur={}
-                  refreshRegions={refreshRegions}
-                  value={}
-                  error={}
-                  canRetry={}
-                />
-              </div>
-              <div className="col-sm-6">
-                <TextInput
-                  title="Zip / Postal Code"
-                  name="postalCode"
-                  required
-                  disabled={addressDisabled}
-                  onChange={}
-                  onBlur={}
-                  value={}
-                  error={}
-                />
-              </div>
+          <div className="row">
+            <div className="col-sm-12">
+              <TextInput
+                title="Address"
+                name="streetAddress"
+                required
+                maxLength={200}
+                disabled={addressDisabled}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.streetAddress}
+                error={touched.streetAddress && errors.streetAddress || undefined}
+              />
             </div>
           </div>
-        )
-      }
-    </>
+          <div className="row">
+            <div className="col-sm-12">
+              <TextInput
+                name="extendedAddress"
+                maxLength={100}
+                disabled={addressDisabled}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.extendedAddress}
+                error={touched.extendedAddress && errors.extendedAddress || undefined}
+              />
+            </div>
+          </div>
+          {
+            countryName && countryName !== 'US'
+            ? (
+              <>
+                <div className="row">
+                  <div className="col-sm-12">
+                    <TextInput
+                      name="intAddressLine3"
+                      maxLength={100}
+                      disabled={addressDisabled}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.intAddressLine3}
+                      error={touched.intAddressLine3 && errors.intAddressLine3 || undefined}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-12">
+                    <TextInput
+                      name="intAddressLine4"
+                      maxLength={100}
+                      disabled={addressDisabled}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.intAddressLine4}
+                      error={touched.intAddressLine4 && errors.intAddressLine4 || undefined}
+                    />
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="row">
+                  <div className="col-sm-12">
+                    <TextInput
+                      title="City"
+                      name="locality"
+                      required
+                      maxLength={50}
+                      disabled={addressDisabled}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.locality}
+                      error={touched.locality && errors.locality || undefined}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-6">
+                    <RegionSelect
+                      addressDisabled={addressDisabled}
+                      regions={regions.map(region => ({ name: region.name, displayName: region['display-name']}))}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      refreshRegions={refreshRegions}
+                      value={values.region}
+                      error={touched.region && errors.region || undefined}
+                      canRetry={loadingRegionsError}
+                    />
+                  </div>
+                  <div className="col-sm-6">
+                    <TextInput
+                      title="Zip / Postal Code"
+                      name="postalCode"
+                      required
+                      disabled={addressDisabled}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.postalCode}
+                      error={touched.postalCode && errors.postalCode || undefined}
+                    />
+                  </div>
+                </div>
+              </>
+            )
+          }
+        </>
+      )}
+    </Formik>
   );
 };
 
