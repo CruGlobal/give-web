710f5486059f4e80d5955749e806eb8c9606df2d
Robert Eldredge <robert.eldredge@cru.org>
2022-03-09 08:58:39 -0500

    Create TextInput Component

---
+++ src/common/components/form/textInput/index.tsx
@@ -0,0 +1,52 @@
+import React, { ChangeEvent, FocusEvent, HTMLInputTypeAttribute } from 'react';
+
+interface TextInputProps {
+  title?: string,
+  type?: HTMLInputTypeAttribute,
+  name: string,
+  required?: boolean,
+  maxLength?: number,
+  pattern?: string,
+  disabled?: boolean,
+  onChange: (event: ChangeEvent<any>) => void,
+  onBlur: (event: FocusEvent<any>) => void,
+  value?: string,
+  error?: string,
+}
+
+const TextInput = ({
+  title,
+  type = 'text',
+  name,
+  required = false,
+  maxLength,
+  pattern,
+  disabled,
+  onChange,
+  onBlur,
+  value,
+  error,
+}: TextInputProps) => (
+  <div className={`form-group${required && ' is-required' || ''}${error && ' has-error' || ''}`}>
+    { title && <label>{title}</label> }
+    <input
+      type={type}
+      className="form-control form-control-subtle"
+      name={name}
+      required={required}
+      maxLength={maxLength}
+      pattern={pattern}
+      disabled={disabled}
+      onChange={onChange}
+      onBlur={onBlur}
+      value={value}
+    />
+    { error && (
+      <div role="alert">
+        <div className="help-block">{error}</div>
+      </div>
+    )}
+  </div>
+);
+
+export default TextInput;
