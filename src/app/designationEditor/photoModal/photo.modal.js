3b2051586651dcbcb847ef2cd4e8b7f4f78d2970
Bill Randall <william.randall@cru.org>
2023-01-12 15:37:49 -0500

    Query the photos on an interval until we have all of them

--- src/app/designationEditor/photoModal/photo.modal.js
+++ src/app/designationEditor/photoModal/photo.modal.js
@@ -7,27 +7,56 @@ const controllerName = 'photoCtrl'
 
 class ModalInstanceCtrl {
   /* @ngInject */
-  constructor ($timeout, designationNumber, campaignPage, photos, photoLocation, selectedPhoto, designationEditorService) {
+  constructor ($timeout, $scope, designationNumber, campaignPage, photos, photoLocation, selectedPhoto, designationEditorService) {
     this.$timeout = $timeout
+    this.$scope = $scope
     this.designationEditorService = designationEditorService
+
+    this.MAX_RETRIES = 3
 
     this.designationNumber = designationNumber
     this.campaignPage = campaignPage
     this.photoLocation = photoLocation
     this.selectedPhoto = selectedPhoto
     this.photos = photos
-
+    this.currentNumberOfPhotos = this.photos.length
+    this.desiredNumberOfPhotos = this.currentNumberOfPhotos
+    this.intervalTries = 0
     this.maxNumberOfPhotos = 6
+
+    this.$scope.$watch('$ctrl.intervalTries', () => {
+      if (this.intervalTries >= this.MAX_RETRIES && this.intervalId) {
+        clearInterval(this.intervalId)
+      }
+    })
   }
 
   uploadComplete () {
     // refresh photos (set timeout to give Adobe time to add to DAM)
+    this.desiredNumberOfPhotos++
+    this.intervalTries = 0
     this.$timeout(() => {
       this.designationEditorService.getPhotos(this.designationNumber).then((response) => {
         this.photos = response.data
         this.uploading = false
+        this.currentNumberOfPhotos = this.photos.length
+        this.intervalId = setInterval(this.queryPhotos, 3500, this)
       }, angular.noop)
     }, 3500)
+  }
+
+  queryPhotos (ctrl) {
+    if (ctrl.currentNumberOfPhotos && ctrl.desiredNumberOfPhotos) {
+      if (ctrl.desiredNumberOfPhotos > ctrl.currentNumberOfPhotos || ctrl.intervalTries < ctrl.MAX_RETRIES) {
+        ctrl.designationEditorService.getPhotos(ctrl.designationNumber).then((response) => {
+          ctrl.photos = response.data
+          ctrl.currentNumberOfPhotos = ctrl.photos.length
+          ctrl.intervalTries++
+        }, angular.noop)
+      } else {
+        ctrl.intervalTries++
+      }
+    }
   }
 
   addImageToCarousel (photo) {
