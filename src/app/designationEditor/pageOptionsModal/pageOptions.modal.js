630b03173b8cfe26ef1206a7e7aba83930ec21f3
Daniel Bisgrove <daniel@bizz-websites.com>
2023-09-08 17:04:08 -0400

    Removing zero amounts initially on my giving page

--- src/app/designationEditor/pageOptionsModal/pageOptions.modal.js
+++ src/app/designationEditor/pageOptionsModal/pageOptions.modal.js
@@ -12,7 +12,7 @@ class ModalInstanceCtrl {
     this.facebookPixelId = facebookPixelId
 
     this.suggestedAmounts = transform(suggestedAmounts, (result, value, key) => {
-      if (key === 'jcr:primaryType') { return }
+      if (key === 'jcr:primaryType' || !value?.amount) return;
       result.push({
         amount: Number(value.amount),
         description: value.description,
@@ -23,10 +23,9 @@ class ModalInstanceCtrl {
   }
 
   transformSuggestedAmounts () {
-    const onlyAmountsWithValue = this.suggestedAmounts.filter((amount) => amount?.amount);
-    return transform(onlyAmountsWithValue, (result, value, i) => {
+    const filterOutZeroAmounts = this.suggestedAmounts.filter((amount) => amount?.amount);
+    return transform(filterOutZeroAmounts, (result, value, i) => {
       delete value.order
-      value.amount = value.amount || 0
       result[i + 1] = value
     }, {})
   }
