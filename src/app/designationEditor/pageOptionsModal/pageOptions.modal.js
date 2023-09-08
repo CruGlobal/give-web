4d72dc535d4e5862fe9c9bb9cd2a34dceacb2417
Daniel Bisgrove <daniel@bizz-websites.com>
2023-09-08 17:23:47 -0400

    Fixing lint issues

--- src/app/designationEditor/pageOptionsModal/pageOptions.modal.js
+++ src/app/designationEditor/pageOptionsModal/pageOptions.modal.js
@@ -12,7 +12,7 @@ class ModalInstanceCtrl {
     this.facebookPixelId = facebookPixelId
 
     this.suggestedAmounts = transform(suggestedAmounts, (result, value, key) => {
-      if (key === 'jcr:primaryType' || !value?.amount) return;
+      if (key === 'jcr:primaryType' || !value?.amount) return
       result.push({
         amount: Number(value.amount),
         description: value.description,
@@ -23,7 +23,7 @@ class ModalInstanceCtrl {
   }
 
   transformSuggestedAmounts () {
-    const filterOutZeroAmounts = this.suggestedAmounts.filter((amount) => amount?.amount);
+    const filterOutZeroAmounts = this.suggestedAmounts.filter((amount) => amount?.amount)
     return transform(filterOutZeroAmounts, (result, value, i) => {
       delete value.order
       result[i + 1] = value
