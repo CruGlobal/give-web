49664f81dbecd80277c48851c79df90a2c6ab956
Caleb Alldrin <caleb.alldrin@cru.org>
2023-09-12 15:00:41 -0700

    Add datadog rum

---
+++ src/common/datadog.config.js
@@ -0,0 +1,18 @@
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '3937053e-386b-4b5b-ab4a-c83217d2f953',
+    clientToken: process.env.DATADOG_CLIENT_TOKEN,
+    site: 'datadoghq.com',
+    service:'give-web',
+    env: process.env.NODE_ENV,
+    version: process.env.GITHUB_SHA,
+    sessionSampleRate:100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
