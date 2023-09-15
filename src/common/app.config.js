0906cd97e7b3492d67ab437f6cfd88e305f6352b
Caleb Alldrin <caleb.alldrin@cru.org>
2023-09-15 08:49:32 -0700

    Fix lint

--- src/common/app.config.js
+++ src/common/app.config.js
@@ -537,7 +537,7 @@ const appConfig = /* @ngInject */ function (envServiceProvider, $compileProvider
   })
   $translateProvider.preferredLanguage('en')
 
-  window.datadogRum = datadogRum;
+  window.datadogRum = datadogRum
   window.datadogRum && window.datadogRum.init({
     applicationId: '3937053e-386b-4b5b-ab4a-c83217d2f953',
     clientToken: process.env.DATADOG_RUM_CLIENT_TOKEN,
