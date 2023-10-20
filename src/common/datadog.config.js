e79f991d1ac5e2d52a1487fbc665bb5320649a4a
Bizz <56281168+dr-bizz@users.noreply.github.com>
2023-10-20 13:45:44 -0400

    Preventing DD from producing errors when running tests (#1062)
    * Ensure Datadog is only initlised when env var is present.
    
    * Adding new node script to test with logs
    
    * Adding DD tests

--- src/common/datadog.config.js
+++ src/common/datadog.config.js
@@ -2,24 +2,27 @@ import 'angular-environment'
 import { datadogRum } from '@datadog/browser-rum'
 
 const dataDogConfig = /* @ngInject */ function (envServiceProvider) {
-  const config = {
-    applicationId: '3937053e-386b-4b5b-ab4a-c83217d2f953',
-    clientToken: process.env.DATADOG_RUM_CLIENT_TOKEN,
-    site: 'datadoghq.com',
-    service: 'give-web',
-    env: envServiceProvider.get(),
-    version: process.env.GITHUB_SHA,
-    sessionSampleRate: envServiceProvider.is('staging') ? 100 : 10,
-    sessionReplaySampleRate: envServiceProvider.is('staging') ? 100 : 1,
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel: 'mask-user-input'
-  }
+  const clientToken = process.env.DATADOG_RUM_CLIENT_TOKEN
+  if (clientToken) {
+    const config = {
+      applicationId: '3937053e-386b-4b5b-ab4a-c83217d2f953',
+      clientToken,
+      site: 'datadoghq.com',
+      service: 'give-web',
+      env: envServiceProvider.get(),
+      version: process.env.GITHUB_SHA,
+      sessionSampleRate: envServiceProvider.is('staging') ? 100 : 10,
+      sessionReplaySampleRate: envServiceProvider.is('staging') ? 100 : 1,
+      trackUserInteractions: true,
+      trackResources: true,
+      trackLongTasks: true,
+      defaultPrivacyLevel: 'mask-user-input'
+    }
 
-  window.datadogRum = datadogRum
-  window.datadogRum && window.datadogRum.init(config)
-  window.datadogRum && window.datadogRum.startSessionReplayRecording()
+    window.datadogRum = datadogRum
+    window.datadogRum && window.datadogRum.init(config)
+    window.datadogRum && window.datadogRum.startSessionReplayRecording()
+  }
 }
 export {
   dataDogConfig as default
