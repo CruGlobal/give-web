09256f404a934a0a4b40d37b21b4ef3757b7bae6
Caleb Cox <canac@users.noreply.github.com>
2023-11-08 14:13:02 -0600

    Only expose giving links to national staff (#1073)
    * Only expose giving links to national staff
    
    * Avoid var

--- src/app/designationEditor/personalOptionsModal/personalOptions.spec.js
+++ src/app/designationEditor/personalOptionsModal/personalOptions.spec.js
@@ -4,21 +4,25 @@ import module from './personalOptions.modal'
 
 describe('Designation Editor Personal Options', function () {
   beforeEach(angular.mock.module(module.name))
-  var $ctrl
+  let $ctrl
+
+  const controllerInjections = {
+    designationNumber: '000555',
+    designationType: 'Staff',
+    giveDomain: 'https://give.cru.org',
+    givingLinks: {
+      'jcr:primaryType': 'nt:unstructured',
+      1: { 'jcr:primaryType': 'nt:unstructured', url: 'https://example.com', name: 'Name' }
+    },
+    showNewsletterForm: true,
+    hasNewsletter: true,
+  }
 
   beforeEach(inject(function ($rootScope, $controller) {
     const $scope = $rootScope.$new()
 
     $ctrl = $controller(module.name, {
-      designationNumber: '000555',
-      designationType: 'Staff',
-      giveDomain: 'https://give.cru.org',
-      givingLinks: {
-        'jcr:primaryType': 'nt:unstructured',
-        1: { 'jcr:primaryType': 'nt:unstructured', url: 'https://example.com', name: 'Name' }
-      },
-      showNewsletterForm: true,
-      hasNewsletter: true,
+      ...controllerInjections,
       $scope: $scope
     })
     $scope.$close = jest.fn()
@@ -38,6 +42,28 @@ describe('Designation Editor Personal Options', function () {
   it('transforms giving links', function () {
     expect($ctrl.transformGivingLinks()).toEqual({
       1: { name: 'Name', url: 'https://example.com' }
+    })
+  })
+
+  describe('givingLinksAvailable', () => {
+    describe('for national staff', () => {
+      beforeEach(inject(function ($rootScope, $controller) {
+        const $scope = $rootScope.$new()
+
+        $ctrl = $controller(module.name, {
+          ...controllerInjections,
+          designationType: 'National Staff',
+          $scope: $scope
+        })
+      }))
+
+      it('is true', () => {
+        expect($ctrl.givingLinksAvailable).toBe(true)
+      })
+    })
+
+    it('otherwise is false', () => {
+      expect($ctrl.givingLinksAvailable).toBe(false)
     })
   })
 
