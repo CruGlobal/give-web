5a7b393ccab47299f9e4af60ebe60344908c6cf9
Matt Drees <matt.drees@cru.org>
2023-11-13 10:28:42 -0700

    Fix some incorrect spec verbiage
    Probably left over copy/paste from the rollbar specs.

--- src/common/datadog.config.spec.js
+++ src/common/datadog.config.spec.js
@@ -4,7 +4,7 @@ import * as module from './datadog.config'
 
 describe('dataDogConfig', () => {
   process.env.DATADOG_RUM_CLIENT_TOKEN = '1234'
-  describe('pipe $log to Rollbar', () => {
+  describe('configure Datadog RUM', () => {
     beforeEach(() => {
       // Use rollbar config function somewhere
       angular.module('testDataDogConfig', ['environment'])
@@ -14,7 +14,7 @@ describe('dataDogConfig', () => {
         inject(() => {})
     })
 
-    it('should send $log.log to rollbar and call through to $log', () => {
+    it('should call init() and start session reply recording', () => {
       expect(window.datadogRum).not.toEqual(undefined)
       expect(window.datadogRum.init).toEqual(expect.any(Function))
       expect(window.datadogRum.startSessionReplayRecording).toEqual(expect.any(Function))
