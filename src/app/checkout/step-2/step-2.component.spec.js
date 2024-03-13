4d1515fe7a12a57bf277f1ca60d2ed41288efb92
Caleb Cox <canac@users.noreply.github.com>
2024-03-13 14:43:55 -0500

    Remove useless assertions

--- src/app/checkout/step-2/step-2.component.spec.js
+++ src/app/checkout/step-2/step-2.component.spec.js
@@ -121,21 +121,17 @@ describe('checkout', () => {
 
     describe('handlePaymentChange', () => {
       it('should change default payment type with bank account', () => {
-        jest.spyOn(self.controller, 'handlePaymentChange')
         jest.spyOn(self.controller.brandedAnalyticsFactory, 'savePaymentType')
         self.controller.handlePaymentChange({'account-type': 'checking'})
 
-        expect(self.controller.handlePaymentChange).toHaveBeenCalledWith({'account-type': 'checking'})
         expect(self.controller.defaultPaymentType).toEqual('checking')
         expect(self.controller.brandedAnalyticsFactory.savePaymentType).toHaveBeenCalledWith('checking', false)
       })
 
       it('should change default payment type with credit card', () => {
-        jest.spyOn(self.controller, 'handlePaymentChange')
         jest.spyOn(self.controller.brandedAnalyticsFactory, 'savePaymentType')
         self.controller.handlePaymentChange({'card-type': 'visa'})
 
-        expect(self.controller.handlePaymentChange).toHaveBeenCalledWith({'card-type': 'visa'})
         expect(self.controller.defaultPaymentType).toEqual('visa')
         expect(self.controller.brandedAnalyticsFactory.savePaymentType).toHaveBeenCalledWith('visa', true)
       })
