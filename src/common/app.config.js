d9ceec6c01aa4eac972893acb6c1cbf876279a45
Daniel Bisgrove <daniel@bizz-websites.com>
2024-10-17 08:33:03 -0400

    Adding recaptchaKey to staging variables to see if that helps dix the reCaptcha issue on staging checkout

--- src/common/app.config.js
+++ src/common/app.config.js
@@ -54,6 +54,7 @@ export const appConfig = /* @ngInject */ function (envServiceProvider, $compileP
         publicCru: 'https://stage.cru.org',
         publicGive: 'https://give-stage2.cru.org',
         acsUrl: 'https://cru-mkt-stage1.adobe-campaign.com/lp/LP63?_uuid=f1938f90-38ea-41a6-baad-9ac133f6d2ec&service=%404k83N_C5RZnLNvwz7waA2SwyzIuP6ATcN8vJjmT5km0iZPYKUUYk54sthkZjj-hltAuOKDYocuEi5Pxv8BSICoA4uppcvU_STKCzjv9RzLpE4hqj&pkey=',
+        recaptchaKey: '6LdNz5UlAAAAAPSrzydROuY76yGVIquVQAup69PO',
         isBrandedCheckout: false,
         oktaUrl: 'https://cru.oktapreview.com',
         oktaClientId: '0oa26fp9l9iFxuKJf0h8',
@@ -66,6 +67,7 @@ export const appConfig = /* @ngInject */ function (envServiceProvider, $compileP
         publicCru: 'https://stage-cloud.cru.org',
         publicGive: 'https://give-dev-cloud.cru.org',
         acsUrl: 'https://cru-mkt-stage1.adobe-campaign.com/lp/LP63?_uuid=f1938f90-38ea-41a6-baad-9ac133f6d2ec&service=%404k83N_C5RZnLNvwz7waA2SwyzIuP6ATcN8vJjmT5km0iZPYKUUYk54sthkZjj-hltAuOKDYocuEi5Pxv8BSICoA4uppcvU_STKCzjv9RzLpE4hqj&pkey=',
+        recaptchaKey: '6LdNz5UlAAAAAPSrzydROuY76yGVIquVQAup69PO',
         oktaUrl: 'https://cru.oktapreview.com',
         oktaClientId: '0oa26fp9l9iFxuKJf0h8',
         oktaReferrer: 'https://give-dev-cloud.cru.org'
@@ -77,6 +79,7 @@ export const appConfig = /* @ngInject */ function (envServiceProvider, $compileP
         publicCru: 'https://stage-cloud.cru.org',
         publicGive: 'https://give-stage-cloud.cru.org',
         acsUrl: 'https://cru-mkt-stage1.adobe-campaign.com/lp/LP63?_uuid=f1938f90-38ea-41a6-baad-9ac133f6d2ec&service=%404k83N_C5RZnLNvwz7waA2SwyzIuP6ATcN8vJjmT5km0iZPYKUUYk54sthkZjj-hltAuOKDYocuEi5Pxv8BSICoA4uppcvU_STKCzjv9RzLpE4hqj&pkey=',
+        recaptchaKey: '6LdNz5UlAAAAAPSrzydROuY76yGVIquVQAup69PO',
         oktaUrl: 'https://cru.oktapreview.com',
         oktaClientId: '0oa26fp9l9iFxuKJf0h8',
         oktaReferrer: 'https://give-stage-cloud.cru.org'
@@ -100,6 +103,7 @@ export const appConfig = /* @ngInject */ function (envServiceProvider, $compileP
         publicCru: 'https://stage.cru.org',
         publicGive: 'https://give-stage2.cru.org',
         acsUrl: 'https://cru-mkt-stage1.adobe-campaign.com/lp/LP63?_uuid=f1938f90-38ea-41a6-baad-9ac133f6d2ec&service=%404k83N_C5RZnLNvwz7waA2SwyzIuP6ATcN8vJjmT5km0iZPYKUUYk54sthkZjj-hltAuOKDYocuEi5Pxv8BSICoA4uppcvU_STKCzjv9RzLpE4hqj&pkey=',
+        recaptchaKey: '6LdNz5UlAAAAAPSrzydROuY76yGVIquVQAup69PO',
         isBrandedCheckout: false,
         oktaUrl: 'https://cru.oktapreview.com',
         oktaClientId: '0oa26fp9l9iFxuKJf0h8',
@@ -112,6 +116,7 @@ export const appConfig = /* @ngInject */ function (envServiceProvider, $compileP
         publicCru: 'https://stage.cru.org',
         publicGive: 'https://give-stage2-next.cru.org',
         acsUrl: 'https://cru-mkt-stage1.adobe-campaign.com/lp/LP63?_uuid=f1938f90-38ea-41a6-baad-9ac133f6d2ec&service=%404k83N_C5RZnLNvwz7waA2SwyzIuP6ATcN8vJjmT5km0iZPYKUUYk54sthkZjj-hltAuOKDYocuEi5Pxv8BSICoA4uppcvU_STKCzjv9RzLpE4hqj&pkey=',
+        recaptchaKey: '6LdNz5UlAAAAAPSrzydROuY76yGVIquVQAup69PO',
         isBrandedCheckout: false,
         oktaUrl: 'https://signon.okta.com',
         oktaClientId: '0oa1p8kb6gr1IGGdc0h8',
@@ -124,6 +129,7 @@ export const appConfig = /* @ngInject */ function (envServiceProvider, $compileP
         publicCru: 'https://www.cru.org',
         publicGive: 'https://give-preprod.cru.org',
         acsUrl: 'https://cru-mkt-prod1-m.adobe-campaign.com/lp/LPEmailPrefCenter?_uuid=8831d67a-0d46-406b-8987-fd07c97c4ca7&service=%400fAlW4GPmxXExp8qlx7HDlAM6FSZUd0yYRlQg6HRsO_kglfi0gs650oHPZX6LrOvg7OHoIWWpobOeGZduxdNU_m5alc&pkey=',
+        recaptchaKey: '6LdNz5UlAAAAAPSrzydROuY76yGVIquVQAup69PO',
         oktaUrl: 'https://signon.okta.com',
         oktaClientId: '0oa1p8kb6gr1IGGdc0h8',
         oktaReferrer: 'https://give-preprod.cru.org'
