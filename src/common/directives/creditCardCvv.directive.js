b34a965910e1ec98a0c0cbaae10ad661188ae968
wjames111 <william.james@cru.org>
2024-11-13 12:30:42 -0500

    Move template into directive.

--- src/common/directives/creditCardCvv.directive.js
+++ src/common/directives/creditCardCvv.directive.js
@@ -1,29 +1,28 @@
 import angular from 'angular'
 import * as cruPayments from '@cruglobal/cru-payments/dist/cru-payments'
+import template from '../templates/creditCardCvv.tpl.html'
 
 const directiveName = 'creditCardCvv'
 
 const creditCardCvv = /* @ngInject */ function () {
   const directiveDefinitionObject = {
-    restrict: 'AE',
-    require: ['^ngModel', '^form'],
+    restrict: 'E',
+    templateUrl: template,
     scope: {
       setCvvValid: '&' 
     },
 
-    link: function (scope, element, attributes, controllers) {
-      const ngModel = controllers[0]
-      const formController = controllers[1]
-      const valueModel = formController[attributes.creditCardCvv]
-
-      scope.$watch(() => valueModel.$viewValue, function () {
-        ngModel.$validate()
+    link: function (scope) {
+      const cvvForm = scope.$ctrl.form.cvv
+      
+      scope.$watch(() => cvvForm.$viewValue, function () {
+        console.log('validate')
+        scope.$ctrl.form.cvv.$validate()
       })
 
-      ngModel.$validators.creditCardCvv = function (modelValue) {
+      cvvForm.$validators.creditCardCvv = function (modelValue) {
         const isCvvValid = (cruPayments.creditCard.cvv.validate.minLength(modelValue)) && (cruPayments.creditCard.cvv.validate.maxLength(modelValue))
         scope.setCvvValid({$event: isCvvValid})
-
 				return isCvvValid
       }
     }
