28fdab3a20ba85fc7b757099e0b5a72cd2b47de8
Caleb Alldrin <caleb.alldrin@cru.org>
2024-11-13 14:04:14 -0800

    Add itemConfig binding

--- src/app/branded/branded-checkout.component.js
+++ src/app/branded/branded-checkout.component.js
@@ -180,6 +180,7 @@ export default angular
       premiumCode: '@',
       premiumName: '@',
       premiumImageUrl: '@',
+      itemConfig: '=',
       donorDetailsVariable: '@donorDetails',
       defaultPaymentType: '@',
       hidePaymentTypeOptions: '@',
