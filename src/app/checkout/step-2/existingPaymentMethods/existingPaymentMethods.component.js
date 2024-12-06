012f3f5a8e2be500632fa227951b93174131f434
wjames111 <william.james@cru.org>
2024-12-06 17:14:32 -0500

    remove console.log.

--- src/app/checkout/step-2/existingPaymentMethods/existingPaymentMethods.component.js
+++ src/app/checkout/step-2/existingPaymentMethods/existingPaymentMethods.component.js
@@ -156,7 +156,6 @@ class ExistingPaymentMethodsController {
     if (this.selectedPaymentMethod?.['card-type'] && this.creditCardPaymentForm?.securityCode) {
       const selectedUri = this.selectedPaymentMethod.self.uri
       const storage = JSON.parse(this.sessionStorage.getItem('storedCvvs'))
-      console.log(storage)
       const getSelectedCvv = storage ? storage[Object.keys(storage).filter((item) => item === selectedUri)] || '' : ''
       // Set CVV to new credit card CVV
       this.creditCardPaymentForm.securityCode.$setViewValue(getSelectedCvv)
