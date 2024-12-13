c92ebd83cd6370b043b99d46beb2bdfdc9ef1422
wjames111 <william.james@cru.org>
2024-12-13 18:28:14 -0500

    Add styles for creditCardCvv.directive.

--- src/common/directives/creditCardCvv.directive.js
+++ src/common/directives/creditCardCvv.directive.js
@@ -1,23 +1,27 @@
 import angular from 'angular'
 const directiveName = 'creditCardCvv'
 
+import './creditCardCvv.directive.scss'
+
 const template =
-`<div class="form-group" ng-class="{'has-error': ($ctrl.creditCardPaymentForm.securityCode | showErrors), 'is-required': !$ctrl.paymentMethod}">
-  <label translate>{{'SEC_CODE'}}</label>
-  <input type="text" 
-    name="securityCode"
-    autocomplete="cc-csc"
-    class="form-control form-control-subtle"
-    ng-model="$ctrl.creditCardPayment.securityCode"
-    ng-required="!$ctrl.paymentMethod || $ctrl.creditCardPayment.cardNumber"
-    ng-attr-placeholder="{{$ctrl.paymentMethod && !$ctrl.creditCardPayment.cardNumber ? '***' : ''}}">
-  <div role="alert" ng-messages="$ctrl.creditCardPaymentForm.securityCode.$error" ng-if="($ctrl.creditCardPaymentForm.securityCode | showErrors)">
-    <div class="help-block" ng-message="required" translate>{{'CARD_SEC_CODE_ERROR'}}</div>
-    <div class="help-block" ng-message="minLength" translate>{{'MIN_LENGTH_CARD_SEC_CODE'}}</div>
-    <div class="help-block" ng-message="maxLength" translate>{{'MAX_LENGTH_CARD_SEC_CODE'}}</div>
-    <div class="help-block" ng-message="cardTypeLength" ng-init="isAmex = $ctrl.cardInfo.type($ctrl.creditCardPayment.cardNumber) === 'American Express'">
+`<div class="credit-card-cvv-container form-group" ng-class="{'has-error': ($ctrl.creditCardPaymentForm.securityCode | showErrors), 'is-required': !$ctrl.paymentMethod}">
+  <span class="credit-card-cvv-label" translate>{{'SEC_CODE'}}</span>
+  <div>
+    <input type="text" 
+      name="securityCode"
+      autocomplete="cc-csc"
+      class="form-control form-control-subtle"
+      ng-model="$ctrl.creditCardPayment.securityCode"
+      ng-required="!$ctrl.paymentMethod || $ctrl.creditCardPayment.cardNumber"
+      ng-attr-placeholder="{{$ctrl.paymentMethod && !$ctrl.creditCardPayment.cardNumber ? '***' : ''}}">
+    <div role="alert" ng-messages="$ctrl.creditCardPaymentForm.securityCode.$error" ng-if="($ctrl.creditCardPaymentForm.securityCode | showErrors)">
+      <div class="credit-card-cvv-help-block help-block" ng-message="required" translate>{{'CARD_SEC_CODE_ERROR'}}</div>
+      <div class="credit-card-cvv-help-block help-block" ng-message="minLength" translate>{{'MIN_LENGTH_CARD_SEC_CODE'}}</div>
+      <div class="credit-card-cvv-help-block help-block" ng-message="maxLength" translate>{{'MAX_LENGTH_CARD_SEC_CODE'}}</div>
+      <div class="credit-card-cvv-help-block help-block" ng-message="cardTypeLength" ng-init="isAmex = $ctrl.cardInfo.type($ctrl.creditCardPayment.cardNumber) === 'American Express'">
       <translate ng-if="!isAmex">{{'LOCATION_OF_CODE_OTHER'}}</translate>
       <translate ng-if="isAmex">{{'LOCATION_OF_CODE_AMEX'}}</translate>
+      </div>
     </div>
   </div>
 </div>`
