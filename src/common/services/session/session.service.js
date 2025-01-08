dba47ba5ff3b4b0d8a6fd6c1b653ac8f82d5fdf9
Daniel Bisgrove <daniel@bizz-websites.com>
2025-01-08 14:03:33 -0500

    fixup! convert login functions to use Observables instead of async/await

--- src/common/services/session/session.service.js
+++ src/common/services/session/session.service.js
@@ -94,28 +94,28 @@ const session = /* @ngInject */ function ($cookies, $rootScope, $http, $timeout,
     signOutWithoutRedirectToOkta: signOutWithoutRedirectToOkta
   }
 
-  function handleOktaRedirect(lastPurchaseId) {
+  function handleOktaRedirect (lastPurchaseId) {
     if (authClient.isLoginRedirect()) {
-        return new Observable(observer => {
-          authClient.token.parseFromUrl()
-            .then((tokenResponse) => {
-              authClient.tokenManager.setTokens(tokenResponse.tokens);
-              return signIn(lastPurchaseId).subscribe({
-                next: result => {
-                  observer.next(result);
-                  observer.complete();
-                },
-                error: err => {
-                  observer.error(err);
-                }
-              });
+      return new Observable(observer => {
+        authClient.token.parseFromUrl()
+          .then((tokenResponse) => {
+            authClient.tokenManager.setTokens(tokenResponse.tokens)
+            return signIn(lastPurchaseId).subscribe({
+              next: result => {
+                observer.next(result)
+                observer.complete()
+              },
+              error: err => {
+                observer.error(err)
+              }
             })
-            .catch(err => {
-                observer.error(err);
-            });
-        });
+          })
+          .catch(err => {
+            observer.error(err)
+          })
+      })
     } else {
-        return Observable.of(false);
+      return Observable.of(false)
     }
   }
 
@@ -124,19 +124,19 @@ const session = /* @ngInject */ function ($cookies, $rootScope, $http, $timeout,
     return new Observable(observer => {
       internalSignIn(lastPurchaseId)
         .finally(() => {
-          $rootScope.$broadcast(SignInEvent);
+          $rootScope.$broadcast(SignInEvent)
         })
         .subscribe({
-            next: response => {
-              const data = response ? response.data : response;
-              observer.next(data);
-              observer.complete();
-            },
-            error: err => {
-              observer.error(err);
-            }
-        });
-    });
+          next: response => {
+            const data = response ? response.data : response
+            observer.next(data)
+            observer.complete()
+          },
+          error: err => {
+            observer.error(err)
+          }
+        })
+    })
   }
 
   function internalSignIn (lastPurchaseId) {
@@ -144,48 +144,48 @@ const session = /* @ngInject */ function ($cookies, $rootScope, $http, $timeout,
       authClient.isAuthenticated()
         .then(isAuthenticated => {
           if (!isAuthenticated) {
-            setRedirectingOnLogin();
-            return authClient.token.getWithRedirect();
-        }
-        return authClient.tokenManager.getTokens();
-      })
-      .then(tokens => {
-        if (!tokens) {
-          return
-        }
-        const data = { access_token: tokens.accessToken.accessToken };
-        // Only send lastPurchaseId if present and currently public
-        if (angular.isDefined(lastPurchaseId) && currentRole() === Roles.public) {
-          data.lastPurchaseId = lastPurchaseId;
-        }
-        // Add marketing search queries back to URL once returned from Okta
-        const locationSearch = $window.sessionStorage.getItem(locationSearchOnLogin) || '';
-        if (locationSearch) {
+            setRedirectingOnLogin()
+            return authClient.token.getWithRedirect()
+          }
+          return authClient.tokenManager.getTokens()
+        })
+        .then(tokens => {
+          if (!tokens) {
+            return
+          }
+          const data = { access_token: tokens.accessToken.accessToken }
+          // Only send lastPurchaseId if present and currently public
+          if (angular.isDefined(lastPurchaseId) && currentRole() === Roles.public) {
+            data.lastPurchaseId = lastPurchaseId
+          }
+          // Add marketing search queries back to URL once returned from Okta
+          const locationSearch = $window.sessionStorage.getItem(locationSearchOnLogin) || ''
+          if (locationSearch) {
           // eslint-disable-next-line
             const searchQueries = locationSearch.split(/\?|\&/);
-            $window.sessionStorage.removeItem(locationSearchOnLogin);
+            $window.sessionStorage.removeItem(locationSearchOnLogin)
             searchQueries.forEach((searchQuery) => {
-              const [search, value] = searchQuery.split('=');
+              const [search, value] = searchQuery.split('=')
               if (search && value) {
-                $location.search(search, value);
+                $location.search(search, value)
               }
-            });
-        }
-        removeOktaRedirectIndicator();
-        return $http({
+            })
+          }
+          removeOktaRedirectIndicator()
+          return $http({
             method: 'POST',
             url: oktaApiUrl('login'),
             data: data,
             withCredentials: true
-        });
-      })
-      .then(response => {
-          observer.next(response);
-          observer.complete();
-      })
-      .catch(err => {
-        observer.error(err);
-      });
+          })
+        })
+        .then(response => {
+          observer.next(response)
+          observer.complete()
+        })
+        .catch(err => {
+          observer.error(err)
+        })
     })
   }
 
