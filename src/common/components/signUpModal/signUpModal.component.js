0eadb32ddafba1c5427a6d6a646ddc9f7329ab8c
Daniel Bisgrove <daniel@bizz-websites.com>
2025-01-10 07:59:52 -0500

    Add organization fields onto sign up

--- src/common/components/signUpModal/signUpModal.component.js
+++ src/common/components/signUpModal/signUpModal.component.js
@@ -2,6 +2,8 @@ import angular from 'angular'
 import includes from 'lodash/includes'
 import assign from 'lodash/assign'
 import pick from 'lodash/pick'
+import 'rxjs/add/operator/finally'
+import { Observable } from 'rxjs/Observable'
 import OktaSignIn from '@okta/okta-signin-widget'
 import sessionService, { Roles } from 'common/services/session/session.service'
 import orderService from 'common/services/api/order.service'
@@ -13,10 +15,11 @@ const componentName = 'signUpModal'
 
 class SignUpModalController {
   /* @ngInject */
-  constructor ($log, $scope, $location, sessionService, cartService, orderService, envService) {
+  constructor ($log, $scope, $location, $translate, sessionService, cartService, orderService, envService) {
     this.$log = $log
     this.$scope = $scope
     this.$location = $location
+    this.$translate = $translate
     this.sessionService = sessionService
     this.orderService = orderService
     this.cartService = cartService
@@ -28,8 +31,18 @@ class SignUpModalController {
     if (includes([Roles.identified, Roles.registered], this.sessionService.getRole())) {
       this.onStateChange({ state: 'sign-in' })
     }
-
-    this.setUpSignUpWidget()
+    this.currentStep = 1
+    this.donorDetails = {}
+    this.loadDonorDetails().finally(() => {
+      this.setUpSignUpWidget();
+    }).subscribe();
+    
+    this.$translate(['GIVE_AS_INDIVIDUAL', 'GIVE_AS_ORGANIZATION', 'ORGANIZATION_NAME']).then(translations => {
+      this.giveAsIndividualTxt = translations.GIVE_AS_INDIVIDUAL;
+      this.giveAsOrganizationTxt = translations.GIVE_AS_ORGANIZATION;
+      this.organizationNameTxt = translations.ORGANIZATION_NAME;
+    });
+    
 
     if (!this.isInsideAnotherModal) {
       this.cartCount = 0
@@ -48,9 +61,9 @@ class SignUpModalController {
     this.oktaSignInWidget.remove()
   }
 
-  async setUpSignUpWidget () {
-    const donorData = await this.loadDonorDetails()
-    this.currentStep = 1 // Default to step 1
+  setUpSignUpWidget () {
+    const donorData = this.donorDetails ?? {}
+    this.currentStep = 1
 
     this.oktaSignInWidget = new OktaSignIn({
       ...this.sessionService.oktaSignInWidgetDefaultOptions,
@@ -63,49 +76,75 @@ class SignUpModalController {
           const step = this.currentStep || 1
           // Split the form into multiple steps for better user experience.
           // Retain the values entered by the user in each step.
-          // Pre-populate the form fields with existing user details to save time.
+          // Pre-populate the form fields with existing user details.
+
+          const organizationNameField = this.$scope.accountType === 'organization' ? [{
+            name: 'organizationName',
+            type: 'text',
+            'label-top': true,
+            label: this.organizationNameTxt,
+            required: true,
+            maxLength: 50,
+            value: this.$scope.organizationName ?? donorData['organization-name'] ?? ''
+          }] : [];
+
           const steps = {
-            // Step 1: Name and email
+            // Step 1: Name, email and account type
             1: [
               {
                 ...schema[0],
-                value: this.$scope.firstName ?? donorData.name['given-name'] ?? this.sessionService.session.first_name ?? ''
+                value: this.$scope.firstName ?? donorData.name?.['given-name'] ?? this.sessionService.session.first_name ?? ''
               },
               {
                 ...schema[1],
-                value: this.$scope.lastName ?? donorData.name['family-name'] ?? this.sessionService.session.last_name ?? ''
+                value: this.$scope.lastName ?? donorData.name?.['family-name'] ?? this.sessionService.session.last_name ?? ''
               },
               {
                 ...schema[2],
                 value: this.$scope.email ?? donorData.email ?? this.sessionService.session.email ?? ''
-              }
+              },
+              {
+                name: 'accountType',
+                type: 'select',
+                options: {
+                  household: this.giveAsIndividualTxt,
+                  organization: this.giveAsOrganizationTxt,
+                },
+                'label-top': true,
+                label: 'Account Type',
+                required: true,
+                wide: true,
+                value: this.$scope.accountType ?? 'household',
+              },
             ],
-            // Step 2: Address plus phone number
+            // Step 2: Address, phone number and organization name (if applicable)
             2: [
+              ...organizationNameField,
               {
                 ...schema[3],
-                value: this.$scope.streetAddress ?? donorData.mailingAddress.streetAddress ?? ''
+                value: this.$scope.streetAddress ?? donorData.mailingAddress?.streetAddress ?? ''
               },
               {
                 ...schema[4],
-                value: this.$scope.city ?? donorData.mailingAddress.locality ?? ''
+                value: this.$scope.city ?? donorData.mailingAddress?.locality ?? ''
               },
               {
                 ...schema[5],
-                value: this.$scope.state ?? donorData.mailingAddress.region ?? ''
+                value: this.$scope.state ?? donorData.mailingAddress?.region ?? ''
               },
               {
                 ...schema[6],
-                value: this.$scope.zipCode ?? donorData.mailingAddress.postalCode ?? ''
+                value: this.$scope.zipCode ?? donorData.mailingAddress?.postalCode ?? ''
               },
               {
                 ...schema[7],
-                value: this.$scope.countryCode ?? donorData.mailingAddress.country ?? ''
+                value: this.$scope.countryCode ?? donorData.mailingAddress?.country ?? ''
               },
               {
                 ...schema[8],
                 value: this.$scope.primaryPhone ?? donorData['phone-number'] ?? ''
-              }],
+              }
+            ],
             // Step 3: Password
             3: [schema[8]]
           }
@@ -119,7 +158,8 @@ class SignUpModalController {
             Object.assign(this.$scope, {
               firstName: userProfile.firstName,
               lastName: userProfile.lastName,
-              email: userProfile.email
+              email: userProfile.email,
+              accountType: postData.accountType,
             })
             this.$scope.$apply(() => this.goToNextStep())
           } else if (step === 2) {
@@ -129,7 +169,8 @@ class SignUpModalController {
               state: userProfile.state,
               zipCode: userProfile.zipCode,
               countryCode: userProfile.countryCode,
-              primaryPhone: userProfile.primaryPhone
+              primaryPhone: userProfile.primaryPhone,
+              organizationName: postData.organizationName,
             })
             this.$scope.$apply(() => this.goToNextStep())
           } else if (step === 3) {
@@ -218,7 +259,7 @@ class SignUpModalController {
   }
 
   injectBackButton () {
-    // Do't show back button on the first step
+    // Don't show back button on the first step
     if (this.currentStep === 1) {
       return
     }
@@ -236,30 +277,30 @@ class SignUpModalController {
     }
   }
 
-  async loadDonorDetails () {
+  loadDonorDetails () {
     this.loadingDonorDetails = true
-    return new Promise((resolve) => {
-      this.orderService.getDonorDetails().subscribe(
-        (data) => {
-          this.loadingDonorDetails = false
-
+    return new Observable(observer => {
+      this.orderService.getDonorDetails().subscribe({
+        next: data => {
           let donorData = data
-
           const checkoutSavedData = this.sessionService.session.checkoutSavedData
           if (checkoutSavedData) {
             donorData = assign(this.donorDetails, pick(checkoutSavedData, [
-              'name', 'email', 'mailingAddress', 'phone-number'
+              'name', 'email', 'mailingAddress', 'organization-name', 'phone-number'
             ]))
           }
-          resolve(donorData)
+          this.donorDetails = donorData
+          this.loadingDonorDetails = false
+          observer.next(donorData)
+          observer.complete()
         },
-        (error) => {
+        error: error => {
           this.loadingDonorDetails = false
           this.$log.error('Error loading donorDetails.', error)
-          resolve({})
+          observer.error(error)
         }
-      )
-    })
+      })
+    });
   }
 
   // On registration complete we need to send the data to Cortex and then redirect the user to the next step
