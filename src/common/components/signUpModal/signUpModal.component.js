53ffa4d910181590d682e43ccc03d98e2bf3d4ff
Daniel Bisgrove <daniel@bizz-websites.com>
2025-01-14 16:19:38 -0500

    Clean up the signUpModal

--- src/common/components/signUpModal/signUpModal.component.js
+++ src/common/components/signUpModal/signUpModal.component.js
@@ -13,6 +13,10 @@ import cartService from 'common/services/api/cart.service'
 require('assets/okta-sign-in/css/okta-sign-in.min.css')
 
 const componentName = 'signUpModal'
+const signUpButtonText = 'Create an Account'
+const nextButtonText = 'Next'
+const backButtonId = 'backButton'
+const backButtonText = 'Back'
 
 class SignUpModalController {
   /* @ngInject */
@@ -32,34 +36,31 @@ class SignUpModalController {
     if (includes([Roles.identified, Roles.registered], this.sessionService.getRole())) {
       this.onSignIn()
     }
-    this.currentStep = 1
-    this.donorDetails = {}
-    this.signUpErrors = []
+    this.initializeVariables()
+    this.loadTranslations()
     this.loadDonorDetails().finally(() => {
       this.setUpSignUpWidget()
     }).subscribe()
+  }
 
+  $onDestroy () {
+    this.oktaSignInWidget?.remove()
+  }
+
+  initializeVariables() {
+    this.currentStep = 1
+    this.donorDetails = {}
+    this.signUpErrors = []
+    this.isLoading = true
+    this.submitting = false
+  }
+
+  loadTranslations() {
     this.$translate(['GIVE_AS_INDIVIDUAL', 'GIVE_AS_ORGANIZATION', 'ORGANIZATION_NAME']).then(translations => {
       this.giveAsIndividualTxt = translations.GIVE_AS_INDIVIDUAL
       this.giveAsOrganizationTxt = translations.GIVE_AS_ORGANIZATION
       this.organizationNameTxt = translations.ORGANIZATION_NAME
     })
-
-    if (!this.isInsideAnotherModal) {
-      this.cartCount = 0
-      this.getTotalQuantitySubscription = this.cartService.getTotalQuantity().subscribe(count => {
-        this.cartCount = count
-      }, () => {
-        this.cartCount = 0
-      })
-    }
-
-    this.isLoading = true
-    this.submitting = false
-  }
-
-  $onDestroy () {
-    this.oktaSignInWidget?.remove()
   }
 
   setUpSignUpWidget () {
@@ -73,185 +74,227 @@ class SignUpModalController {
       },
       flow: 'signup',
       registration: {
-        parseSchema: (schema, onSuccess) => {
-          const step = this.currentStep || 1
-          // Split the form into multiple steps for better user experience.
-          // Retain the values entered by the user in each step.
-          // Pre-populate the form fields with existing user details.
-
-          const organizationNameField = this.$scope.accountType === 'organization'
-            ? [{
-                name: 'organizationName',
-                type: 'text',
-                'label-top': true,
-                label: this.organizationNameTxt,
-                required: true,
-                maxLength: 50,
-                value: this.$scope.organizationName ?? donorData['organization-name'] ?? ''
-              }]
-            : []
-
-          const steps = {
-            // Step 1: Name, email and account type
-            1: [
-              {
-                ...schema[0],
-                value: this.$scope.firstName ?? donorData.name?.['given-name'] ?? this.sessionService.session.first_name ?? ''
-              },
-              {
-                ...schema[1],
-                value: this.$scope.lastName ?? donorData.name?.['family-name'] ?? this.sessionService.session.last_name ?? ''
-              },
-              {
-                ...schema[2],
-                value: this.$scope.email ?? donorData.email ?? this.sessionService.session.email ?? ''
-              },
-              {
-                name: 'accountType',
-                type: 'select',
-                options: {
-                  Household: this.giveAsIndividualTxt,
-                  Organization: this.giveAsOrganizationTxt
-                },
-                'label-top': true,
-                label: 'Account Type',
-                required: true,
-                wide: true,
-                value: this.$scope.accountType ?? donorData['donor-type'] ?? 'household'
-              }
-            ],
-            // Step 2: Address, phone number and organization name (if applicable)
-            2: [
-              ...organizationNameField,
-              {
-                ...schema[3],
-                value: this.$scope.streetAddress ?? donorData.mailingAddress?.streetAddress ?? ''
-              },
-              {
-                ...schema[4],
-                value: this.$scope.city ?? donorData.mailingAddress?.locality ?? ''
-              },
-              {
-                ...schema[5],
-                value: this.$scope.state ?? donorData.mailingAddress?.region ?? ''
-              },
-              {
-                ...schema[6],
-                value: this.$scope.zipCode ?? donorData.mailingAddress?.postalCode ?? ''
-              },
-              {
-                ...schema[7],
-                value: this.$scope.countryCode ?? donorData.mailingAddress?.country ?? ''
-              },
-              {
-                ...schema[8],
-                value: this.$scope.primaryPhone ?? donorData['phone-number'] ?? ''
-              }
-            ],
-            // Step 3: Password
-            3: [schema[9]]
-          }
-          onSuccess(steps[step])
-        },
-        preSubmit: (postData, onSuccess) => {
-          const step = this.currentStep
-          const userProfile = postData.userProfile
-          if (step === 1) {
-            Object.assign(this.$scope, {
-              firstName: userProfile.firstName,
-              lastName: userProfile.lastName,
-              email: userProfile.email,
-              accountType: postData.accountType
-            })
-            this.$scope.$apply(() => this.goToNextStep())
-          } else if (step === 2) {
-            Object.assign(this.$scope, {
-              streetAddress: userProfile.streetAddress,
-              city: userProfile.city,
-              state: userProfile.state,
-              zipCode: userProfile.zipCode,
-              countryCode: userProfile.countryCode,
-              primaryPhone: userProfile.primaryPhone,
-              organizationName: postData.organizationName
-            })
-            this.$scope.$apply(() => this.goToNextStep())
-          } else if (step === 3) {
-            // Clear errors from previous steps
-            this.signUpErrors = []
-            // Add the user profile to the postData object
-            // Okta widget handles the password
-            postData.userProfile = {
-              firstName: this.$scope.firstName,
-              lastName: this.$scope.lastName,
-              email: this.$scope.email,
-              streetAddress: this.$scope.streetAddress,
-              city: this.$scope.city,
-              state: this.$scope.state,
-              zipCode: this.$scope.zipCode,
-              countryCode: this.$scope.countryCode,
-              primaryPhone: this.$scope.primaryPhone
-            }
-            onSuccess(postData)
-          }
-        },
-        postSubmit: (response, onSuccess) => {
-          const donorDetails = {
-            name: {
-              'given-name': this.$scope.firstName,
-              'family-name': this.$scope.lastName
-            },
-            'donor-type': this.$scope.accountType,
-            'organization-name': this.$scope.organizationName,
-            email: this.$scope.email,
-            phone: this.$scope.primaryPhone,
-            mailingAddress: {
-              streetAddress: this.$scope.streetAddress,
-              locality: this.$scope.city,
-              region: this.$scope.state,
-              postalCode: this.$scope.zipCode,
-              country: this.$scope.countryCode
-            }
-          }
-          this.$scope.$apply(() => this.onSignUp({ donorDetails }))
-
-          onSuccess(response)
-        }
+        parseSchema: this.parseSchema.bind(this),
+        preSubmit: this.preSubmit.bind(this),
+        postSubmit: this.postSubmit.bind(this)
       }
     })
 
     this.signIn()
 
-    this.oktaSignInWidget.on('afterRender', this.afterRenderFn.bind(this))
-    this.oktaSignInWidget.on('afterError', this.afterErrorFn.bind(this))
+    this.oktaSignInWidget.on('afterRender', this.afterRender.bind(this))
+    this.oktaSignInWidget.on('afterError', this.afterError.bind(this))
   }
 
-  afterErrorFn (_, error) {
+  parseSchema(schema, onSuccess) {
+    // Split the form into multiple steps for better user experience.
+    const step = this.currentStep || 1
+    const steps = this.getSteps(schema)
+    onSuccess(steps[step])
+  }
+
+
+  getSteps(schema) {
+    return {
+      // Step 1: Name, email and account type
+      1: this.getStep1Fields(schema),
+      // Step 2: Address, phone number and organization name (if applicable)
+      2: this.getStep2Fields(schema),
+      // Step 3: Password (We don't save the password for security reasons.
+      // Which is why it's the last step)
+      3: [schema[9]]
+    }
+  }
+
+  getStep1Fields(schema) {
+    // Retain the values entered by the user when navigating between steps.
+    // Pre-populate the form fields with existing user details.
+    return [
+      {
+        ...schema[0],
+        value: this.$scope.firstName ?? this.donorDetails?.name?.['given-name'] ?? this.sessionService.session.first_name ?? ''
+      },
+      {
+        ...schema[1],
+        value: this.$scope.lastName ?? this.donorDetails?.name?.['family-name'] ?? this.sessionService.session.last_name ?? ''
+      },
+      {
+        ...schema[2],
+        value: this.$scope.email ?? this.donorDetails?.email ?? this.sessionService.session.email ?? ''
+      },
+      {
+        name: 'accountType',
+        type: 'select',
+        options: {
+          Household: this.giveAsIndividualTxt,
+          Organization: this.giveAsOrganizationTxt
+        },
+        'label-top': true,
+        label: 'Account Type',
+        required: true,
+        wide: true,
+        value: this.$scope.accountType ?? this.donorDetails?.['donor-type'] ?? 'Household'
+      }
+    ]
+  }
+
+  getStep2Fields(schema) {
+    // Retain the values entered by the user when navigating between steps.
+    // Pre-populate the form fields with existing user details.
+
+    const organizationNameField = this.$scope.accountType === 'organization'
+    ? [{
+        name: 'organizationName',
+        type: 'text',
+        'label-top': true,
+        label: this.organizationNameTxt,
+        required: true,
+        maxLength: 50,
+        value: this.$scope.organizationName ?? this.donorDetails?.['organization-name'] ?? ''
+      }]
+    : []
+
+    return [
+      ...organizationNameField,
+      {
+        ...schema[3],
+        value: this.$scope.streetAddress ?? this.donorDetails?.mailingAddress?.streetAddress ?? ''
+      },
+      {
+        ...schema[4],
+        value: this.$scope.city ?? this.donorDetails?.mailingAddress?.locality ?? ''
+      },
+      {
+        ...schema[5],
+        value: this.$scope.state ?? this.donorDetails?.mailingAddress?.region ?? ''
+      },
+      {
+        ...schema[6],
+        value: this.$scope.zipCode ?? this.donorDetails?.mailingAddress?.postalCode ?? ''
+      },
+      {
+        ...schema[7],
+        value: this.$scope.countryCode ?? this.donorDetails?.mailingAddress?.country ?? ''
+      },
+      {
+        ...schema[8],
+        value: this.$scope.primaryPhone ?? this.donorDetails?.['phone-number'] ?? ''
+      }
+    ]
+  }
+
+  preSubmit(postData, onSuccess) {
+    const step = this.currentStep
+    const userProfile = postData.userProfile
+    if (step === 1) {
+      this.saveStep1Data(userProfile, postData)
+    } else if (step === 2) {
+      this.saveStep2Data(userProfile, postData)
+    } else if (step === 3) {
+      this.submitFinalData(postData, onSuccess)
+    }
+  }
+
+  saveStep1Data(userProfile, postData) {
+    Object.assign(this.$scope, {
+      firstName: userProfile.firstName,
+      lastName: userProfile.lastName,
+      email: userProfile.email,
+      accountType: postData.accountType
+    })
+    this.$scope.$apply(() => this.goToNextStep())
+  }
+
+  saveStep2Data(userProfile, postData) {
+    Object.assign(this.$scope, {
+      streetAddress: userProfile.streetAddress,
+      city: userProfile.city,
+      state: userProfile.state,
+      zipCode: userProfile.zipCode,
+      countryCode: userProfile.countryCode,
+      primaryPhone: userProfile.primaryPhone,
+      organizationName: postData.organizationName
+    })
+    this.$scope.$apply(() => this.goToNextStep())
+  }
+
+  submitFinalData(postData, onSuccess) {
+     // Clear errors from previous steps
+    this.signUpErrors = []
+    // Add the user profile to the postData object
+    // Okta widget handles the password
+    postData.userProfile = {
+      firstName: this.$scope.firstName,
+      lastName: this.$scope.lastName,
+      email: this.$scope.email,
+      streetAddress: this.$scope.streetAddress,
+      city: this.$scope.city,
+      state: this.$scope.state,
+      zipCode: this.$scope.zipCode,
+      countryCode: this.$scope.countryCode,
+      primaryPhone: this.$scope.primaryPhone
+    }
+    onSuccess(postData)
+  }
+
+  postSubmit(response, onSuccess) {
+    const donorDetails = {
+      name: {
+        'given-name': this.$scope.firstName,
+        'family-name': this.$scope.lastName
+      },
+      'donor-type': this.$scope.accountType,
+      'organization-name': this.$scope.organizationName,
+      email: this.$scope.email,
+      phone: this.$scope.primaryPhone,
+      mailingAddress: {
+        streetAddress: this.$scope.streetAddress,
+        locality: this.$scope.city,
+        region: this.$scope.state,
+        postalCode: this.$scope.zipCode,
+        country: this.$scope.countryCode
+      }
+    }
+    this.$scope.$apply(() => this.onSignUp({ donorDetails }))
+    onSuccess(response)
+  }
+
+  afterError (_, error) {
     // Save errors to local variable to inject into the form
     // Since errors are cleared on each step change
     this.signUpErrors = error.xhr.responseJSON.errorCauses
   }
 
-  afterRenderFn (context) {
+  afterRender (context) {
+    this.updateSignUpButtonText()
+    this.resetCurrentStepOnRegistrationComplete(context)
+    this.redirectToSignInModalIfNeeded(context)
+    this.injectErrorMessages()
+    this.injectBackButton()
+  }
+
+  updateSignUpButtonText() {
     // Change the text of the sign up button to ensure it's clear what the user is doing
     const signUpButton = angular.element(document.querySelector('.o-form-button-bar input.button.button-primary'))
-    if (this.currentStep === 3) {
-      signUpButton.attr('value', 'Create an Account')
-    } else {
-      signUpButton.attr('value', 'Next')
-    }
+    signUpButton.attr('value', this.currentStep === 3 ? signUpButtonText : nextButtonText)
+  }
 
+  resetCurrentStepOnRegistrationComplete(context) {
     // Stop tracking the current step after registration is complete
     if (context.controller === 'registration-complete') {
       this.currentStep = null
-      console.log('Registration complete')
     }
+  }
 
+  redirectToSignInModalIfNeeded(context) {
     // Send users to the login modal if they try to go to the login form
     if (context.controller === 'primary-auth') {
       this.$scope.$apply(() => this.onSignIn())
     }
+  }
 
-    // Inject error messages into the form as errors are cleared on each step change
+  injectErrorMessages() {
+    // Inject error messages into the form since errors are cleared when switching steps/rerendering.
     this.signUpErrors.forEach(error => {
       const field = document.querySelector(`.o-form-input-name-${error.property.replace(/\./g, '\\.')}`);
       if (field) {
@@ -263,24 +306,40 @@ class SignUpModalController {
         field.parentNode.appendChild(errorElement);
       }
     });
+  }
 
-    this.injectBackButton()
+  injectBackButton () {
+    // Don't show back button on the first step
+    if (this.currentStep === 1) {
+      return
+    }
+    const buttonBar = document.querySelector('.o-form-button-bar')
+     // Ensure the button is only added once
+    if (buttonBar && !buttonBar.querySelector(`#${backButtonId}`)) {
+      const backButton = angular.element(`<button id="${backButtonId}" class="btn btn-secondary">${backButtonText}</button>`)
+      // Add click behavior to go back a step
+      backButton.on('click', (e) => {
+        e.preventDefault()
+        this.$scope.$apply(() => this.goToPreviousStep())
+      })
+      // Prepend the Back button before the "Next" button
+      angular.element(buttonBar).prepend(backButton)
+    }
   }
 
   goToNextStep () {
-    // Set the current step to the next step
     this.currentStep++
     this.reRenderWidget()
   }
 
   goToPreviousStep () {
-    // Set the current step to the previous step
     this.currentStep = Math.max(this.currentStep - 1, 1)
     this.reRenderWidget()
   }
 
   reRenderWidget () {
     // Render the widget again to show new step
+    // Unfortunately, this removes the error messages, which is why we inject them after rendering
     this.oktaSignInWidget.remove()
     this.oktaSignInWidget.renderEl(
       { el: '#osw-container' },
@@ -301,25 +360,6 @@ class SignUpModalController {
       this.oktaSignInWidget.authClient.handleLoginRedirect(tokens)
     } catch (error) {
       this.$log.error('Error showing Okta sign in widget.', error)
-    }
-  }
-
-  injectBackButton () {
-    // Don't show back button on the first step
-    if (this.currentStep === 1) {
-      return
-    }
-    const buttonBar = document.querySelector('.o-form-button-bar')
-    // Ensure the button is only added once
-    if (buttonBar && !buttonBar.querySelector('#backButton')) {
-      const backButton = angular.element('<button id="backButton" class="btn btn-secondary">Back</button>')
-      // Add click behavior to go back a step
-      backButton.on('click', (e) => {
-        e.preventDefault()
-        this.$scope.$apply(() => this.goToPreviousStep())
-      })
-      // Prepend the Back button before the "Next" button
-      angular.element(buttonBar).prepend(backButton)
     }
   }
 
