b215dc50c09952bec816cc1deada44b305fbccb9
Daniel Bisgrove <daniel@bizz-websites.com>
2025-01-16 15:57:03 -0500

    I have reverted signIn to it's prior state. This is because we no longer needed the changes that we made.

--- src/app/signIn/signIn.component.js
+++ src/app/signIn/signIn.component.js
@@ -5,29 +5,22 @@ import showErrors from 'common/filters/showErrors.filter'
 import analyticsFactory from 'app/analytics/analytics.factory'
 import sessionService, { Roles } from 'common/services/session/session.service'
 import sessionModalService from 'common/services/session/sessionModal.service'
-import orderService from 'common/services/api/order.service'
 import template from './signIn.tpl.html'
 
 const componentName = 'signIn'
 
 class SignInController {
   /* @ngInject */
-  constructor ($window, sessionService, analyticsFactory, sessionModalService, orderService) {
+  constructor ($window, sessionService, analyticsFactory, sessionModalService) {
     this.$window = $window
     this.sessionService = sessionService
     this.analyticsFactory = analyticsFactory
     this.sessionModalService = sessionModalService
-    this.orderService = orderService
   }
 
   $onInit () {
     this.subscription = this.sessionService.sessionSubject.subscribe(() => this.sessionChanged())
     this.analyticsFactory.pageLoaded()
-    if (this.sessionService.hasLocationOnLogin() && this.sessionService.getRole() === Roles.registered) {
-      this.showRedirectingLoadingIcon = true
-    } else {
-      this.showRedirectingLoadingIcon = false
-    }
   }
 
   $onDestroy () {
@@ -35,36 +28,9 @@ class SignInController {
   }
 
   sessionChanged () {
-    if (this.sessionService.getRole() !== Roles.registered) {
-      return
+    if (this.sessionService.getRole() === Roles.registered) {
+      this.$window.location = `/checkout.html${window.location.search}`
     }
-
-    const redirectToLocationPriorToLogin = () => {
-      this.showRedirectingLoadingIcon = true
-      const locationToReturnUser = this.sessionService.hasLocationOnLogin()
-      if (locationToReturnUser) {
-        this.sessionService.removeLocationOnLogin()
-        this.$window.location = locationToReturnUser
-      } else {
-        this.$window.location = `/checkout.html${window.location.search}`
-      }
-    }
-
-    this.orderService.getDonorDetails()
-      .subscribe((data) => {
-        const registrationState = data['registration-state']
-        if (registrationState === 'NEW' || registrationState === 'MATCHED') {
-          this.showRedirectingLoadingIcon = false
-          const modal = registrationState === 'NEW'
-            ? this.sessionModalService.registerAccount()
-            : this.sessionModalService.userMatch()
-          modal && modal.then(() => {
-            redirectToLocationPriorToLogin()
-          })
-        } else {
-          redirectToLocationPriorToLogin()
-        }
-      })
   }
 
   checkoutAsGuest () {
@@ -81,10 +47,6 @@ class SignInController {
   onSignUpWithOkta () {
     this.sessionModalService.createAccount()
   }
-
-  closeRedirectingLoading () {
-    this.showRedirectingLoadingIcon = false
-  }
 }
 
 export default angular
@@ -93,7 +55,6 @@ export default angular
     analyticsFactory.name,
     sessionService.name,
     sessionModalService.name,
-    orderService.name,
     signInForm.name,
     showErrors.name
   ])
--- src/app/signIn/signIn.spec.js
+++ src/app/signIn/signIn.spec.js
@@ -7,31 +7,21 @@ import 'rxjs/add/observable/throw'
 
 describe('signIn', function () {
   beforeEach(angular.mock.module(module.name))
-  let $ctrl, orderService
+  let $ctrl
 
-  beforeEach(inject(function (_$componentController_, _orderService_) {
-    orderService = _orderService_
+  beforeEach(inject(function (_$componentController_) {
     $ctrl = _$componentController_(module.name,
-      { $window: {
-          location: '/sign-in.html',
-          localStorage: {
-            getItem: jest.fn(),
-            removeItem: jest.fn(),
-          }
-        }
-      }
+      { $window: { location: '/sign-in.html' } }
     )
   }))
-  
+
   it('to be defined', function () {
     expect($ctrl).toBeDefined()
   })
 
   describe('as \'GUEST\'', () => {
     beforeEach(() => {
-      jest.spyOn($ctrl.sessionService, 'hasLocationOnLogin').mockReturnValue('https://give-stage2.cru.org/search-results.html')
       jest.spyOn($ctrl.sessionService, 'getRole').mockReturnValue('GUEST')
-      jest.spyOn($ctrl.sessionService, 'removeLocationOnLogin')
       jest.spyOn($ctrl, 'sessionChanged')
       $ctrl.$onInit()
     })
@@ -44,18 +34,10 @@ describe('signIn', function () {
       expect($ctrl.sessionChanged).toHaveBeenCalled()
       expect($ctrl.$window.location).toEqual('/sign-in.html')
     })
-
-    it('does not show redirecting loading sign when not fully registered', () => {
-      expect($ctrl.sessionChanged).toHaveBeenCalled()
-      expect($ctrl.showRedirectingLoadingIcon).toEqual(false)
-      expect($ctrl.sessionService.removeLocationOnLogin).not.toHaveBeenCalled()
-    })
   })
 
   describe('as \'IDENTIFIED\'', () => {
     beforeEach(() => {
-      jest.spyOn($ctrl.sessionService, 'hasLocationOnLogin').mockReturnValue('https://give-stage2.cru.org/search-results.html')
-      jest.spyOn($ctrl.sessionService, 'removeLocationOnLogin')
       jest.spyOn($ctrl.sessionService, 'getRole').mockReturnValue('IDENTIFIED')
       jest.spyOn($ctrl, 'sessionChanged')
       $ctrl.$onInit()
@@ -68,94 +50,24 @@ describe('signIn', function () {
     it('has does not change location', () => {
       expect($ctrl.sessionChanged).toHaveBeenCalled()
       expect($ctrl.$window.location).toEqual('/sign-in.html')
-      expect($ctrl.sessionService.removeLocationOnLogin).not.toHaveBeenCalled()
-      expect($ctrl.sessionService.hasLocationOnLogin).toHaveBeenCalledTimes(1)
-    })
-
-    it('does not show redirecting loading sign when not fully registered', () => {
-      jest.spyOn($ctrl.sessionService, 'hasLocationOnLogin').mockReturnValue('https://give-stage2.cru.org/search-results.html')
-      $ctrl.$onInit()
-      expect($ctrl.sessionChanged).toHaveBeenCalled()
-      expect($ctrl.showRedirectingLoadingIcon).toEqual(false)
-      expect($ctrl.sessionService.removeLocationOnLogin).not.toHaveBeenCalled()
     })
   })
 
   describe('as \'REGISTERED\'', () => {
     beforeEach(() => {
       jest.spyOn($ctrl.sessionService, 'getRole').mockReturnValue('REGISTERED')
-      jest.spyOn($ctrl.sessionService, 'removeLocationOnLogin')
       jest.spyOn($ctrl, 'sessionChanged')
+      $ctrl.$onInit()
     })
 
     afterEach(() => {
       $ctrl.$onDestroy()
     })
 
-    describe('Without a donor account account', () => {
-      let deferred, $rootScope
-      beforeEach(inject((_$q_, _$rootScope_) => {
-        deferred = _$q_.defer()
-        $rootScope = _$rootScope_
-        jest.spyOn($ctrl.sessionModalService, 'registerAccount').mockReturnValue(deferred.promise)
-        jest.spyOn($ctrl.sessionModalService, 'userMatch').mockReturnValue(deferred.promise)
-      }))
-
-      it('shows register for a donor account modal upon initial Okta sign in', () => {
-        jest.spyOn(orderService, 'getDonorDetails').mockImplementation(() => Observable.of({ 'registration-state': 'NEW' }))
-        jest.spyOn($ctrl.sessionService, 'hasLocationOnLogin').mockReturnValue('https://give-stage2.cru.org/search-results.html')
-        $ctrl.$onInit()
-        expect($ctrl.sessionChanged).toHaveBeenCalled()
-        expect($ctrl.showRedirectingLoadingIcon).toEqual(true)
-        expect($ctrl.sessionModalService.registerAccount).toHaveBeenCalled()
-        expect($ctrl.$window.location).toEqual('/sign-in.html')
-        expect($ctrl.sessionModalService.userMatch).not.toHaveBeenCalled()
-        expect($ctrl.sessionService.removeLocationOnLogin).not.toHaveBeenCalled()
-
-        deferred.resolve()
-        $rootScope.$digest()
-        expect($ctrl.sessionService.removeLocationOnLogin).toHaveBeenCalled()
-      })
-
-      it('shows register a donor account modal upon matched account', () => {
-        jest.spyOn(orderService, 'getDonorDetails').mockImplementation(() => Observable.of({ 'registration-state': 'MATCHED' }))
-        jest.spyOn($ctrl.sessionService, 'hasLocationOnLogin').mockReturnValue('https://give-stage2.cru.org/search-results.html')
-        $ctrl.$onInit()
-        expect($ctrl.sessionChanged).toHaveBeenCalled()
-        expect($ctrl.sessionModalService.userMatch).toHaveBeenCalled()
-        expect($ctrl.$window.location).toEqual('/sign-in.html')
-        expect($ctrl.sessionModalService.registerAccount).not.toHaveBeenCalled()
-        expect($ctrl.sessionService.removeLocationOnLogin).not.toHaveBeenCalled()
-
-        deferred.resolve()
-        $rootScope.$digest()
-        expect($ctrl.sessionService.removeLocationOnLogin).toHaveBeenCalled()
-      })
+    it('navigates to checkout', () => {
+      expect($ctrl.sessionChanged).toHaveBeenCalled()
+      expect($ctrl.$window.location).toEqual('/checkout.html')
     })
-
-    describe('With a donor account account', () => {
-      beforeEach(() => {
-        jest.spyOn(orderService, 'getDonorDetails').mockImplementation(() => Observable.of({ 'registration-state': 'REGISTERED' }))
-      })
-
-      it('navigates to checkout if location prior to login wasn\'t saved', () => {
-        jest.spyOn($ctrl.sessionService, 'hasLocationOnLogin').mockReturnValue(null)
-        $ctrl.$onInit()
-        expect($ctrl.sessionChanged).toHaveBeenCalled()
-        expect($ctrl.sessionService.hasLocationOnLogin).toHaveBeenCalledTimes(2)
-        expect($ctrl.sessionService.removeLocationOnLogin).not.toHaveBeenCalled()
-        expect($ctrl.$window.location).toEqual('/checkout.html')
-      })
-
-      it('navigates to location prior to login', () => {
-        jest.spyOn($ctrl.sessionService, 'hasLocationOnLogin').mockReturnValue('https://give-stage2.cru.org/search-results.html')
-        $ctrl.$onInit()
-        expect($ctrl.sessionChanged).toHaveBeenCalled()
-        expect($ctrl.sessionService.hasLocationOnLogin).toHaveBeenCalledTimes(2)
-        expect($ctrl.sessionService.removeLocationOnLogin).toHaveBeenCalled()
-        expect($ctrl.$window.location).toEqual('https://give-stage2.cru.org/search-results.html')
-      })
-    });
   })
 
   describe('checkoutAsGuest()', () => {
@@ -176,23 +88,5 @@ describe('signIn', function () {
         expect($ctrl.$window.location).toEqual('/checkout.html')
       })
     })
-
-
-    describe('onSignUpWithOkta()', () => {
-      it('should call createAccount()', () => {
-        jest.spyOn($ctrl.sessionModalService, 'createAccount')
-        $ctrl.onSignUpWithOkta()
-
-        expect($ctrl.sessionModalService.createAccount).toHaveBeenCalled()
-      })
-    })
   })
-
-  describe('closeRedirectingLoading()', () => {
-    it('should remove the Redirecting loading icon', () => {
-      $ctrl.showRedirectingLoadingIcon = true
-      $ctrl.closeRedirectingLoading()
-      expect($ctrl.showRedirectingLoadingIcon).toEqual(false)
-    });
-  });
 })
