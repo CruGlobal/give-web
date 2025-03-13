29f305cda37a76df4dc1959de7fe65cb5d1e8a9a
Caleb Cox <canac@users.noreply.github.com>
2025-03-13 12:12:35 -0500

    Sort countries and regions in the geographies service

--- src/common/services/api/geographies.service.spec.js
+++ src/common/services/api/geographies.service.spec.js
@@ -2,6 +2,7 @@ import angular from 'angular'
 import 'angular-mocks'
 import module from './geographies.service'
 import find from 'lodash/find'
+import map from 'lodash/map'
 
 import countriesResponse from 'common/services/api/fixtures/cortex-countries.fixture.js'
 import regionsResponse from 'common/services/api/fixtures/cortex-regions-us.fixture.js'
@@ -21,18 +22,59 @@ describe('geographies service', () => {
   })
 
   describe('getCountries', () => {
-    it('should send a request to get the list of countries', () => {
+    it('should send a request to get the list of countries and sort them alphabetically', (done) => {
       self.$httpBackend.expectGET('https://give-stage2.cru.org/cortex/geographies/crugive/countries?zoom=element').respond(200, countriesResponse)
       self.geographiesService.getCountries()
         .subscribe(data => {
-          expect(data).toEqual(countriesResponse._element)
-        })
+          expect(map(data, 'display-name')).toEqual([
+            'Australia',
+            'Austria',
+            'Belgium',
+            'Canada',
+            'China',
+            'Czech Republic',
+            'Denmark',
+            'Finland',
+            'France',
+            'Germany',
+            'Great Britain (UK)',
+            'Greece',
+            'Hong Kong',
+            'Hungary',
+            'Iceland',
+            'India',
+            'Indonesia',
+            'Ireland',
+            'Israel',
+            'Italy',
+            'Japan',
+            'Korea (South)',
+            'Netherlands',
+            'New Zealand',
+            'Norway',
+            'Pakistan',
+            'Poland',
+            'Portugal',
+            'Romania',
+            'Russian Federation',
+            'Singapore',
+            'South Africa',
+            'Spain',
+            'Sweden',
+            'Switzerland',
+            'Taiwan',
+            'Ukraine',
+            'United States',
+            'Uzbekistan',
+          ])
+          done()
+        }, done)
       self.$httpBackend.flush()
     })
   })
 
   describe('getRegions', () => {
-    it('should send a request to get the list of regions for a country', () => {
+    it('should send a request to get the list of regions for a country and sort them alphabetically', (done) => {
       self.$httpBackend.expectGET('https://give-stage2.cru.org/cortex/geographies/crugive/countries/kvjq=/regions?zoom=element').respond(200, regionsResponse)
       const us = find(countriesResponse._element, { name: 'US' })
       self.geographiesService.getRegions(us)
@@ -42,8 +84,65 @@ describe('geographies service', () => {
             expect.objectContaining({ 'display-name': 'Florida', name: 'FL' })
           ]))
 
-          expect(data.length).toEqual(54)
-        })
+          expect(map(data, 'display-name')).toEqual([
+            'Alabama',
+            'Alaska',
+            'Arizona',
+            'Arkansas',
+            'Armed Forces Americas (AA)',
+            'Armed Forces Europe (AE)',
+            'Armed Forces Pacific (AP)',
+            'California',
+            'Colorado',
+            'Connecticut',
+            'Delaware',
+            'District Of Columbia',
+            'Florida',
+            'Georgia',
+            'Hawaii',
+            'Idaho',
+            'Illinois',
+            'Indiana',
+            'Iowa',
+            'Kansas',
+            'Kentucky',
+            'Louisiana',
+            'Maine',
+            'Maryland',
+            'Massachusetts',
+            'Michigan',
+            'Minnesota',
+            'Mississippi',
+            'Missouri',
+            'Montana',
+            'Nebraska',
+            'Nevada',
+            'New Hampshire',
+            'New Jersey',
+            'New Mexico',
+            'New York',
+            'North Carolina',
+            'North Dakota',
+            'Ohio',
+            'Oklahoma',
+            'Oregon',
+            'Pennsylvania',
+            'Rhode Island',
+            'South Carolina',
+            'South Dakota',
+            'Tennessee',
+            'Texas',
+            'Utah',
+            'Vermont',
+            'Virginia',
+            'Washington',
+            'West Virginia',
+            'Wisconsin',
+            'Wyoming',
+          ])
+
+          done()
+        }, done)
       self.$httpBackend.flush()
     })
 
